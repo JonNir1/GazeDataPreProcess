828ec8a70824f6b34c21ed055e684d1e72016350
JonNir <jonathan.nir@mail.huji.ac.il>
2023-03-08 14:47:26 +0200

    create another blink detector

---
+++ EventDetectors/PupilSizeBlinkDetector.py
@@ -0,0 +1,13 @@
+
+
+from EventDetectors.BaseBlinkDetector import BaseBlinkDetector
+
+
+class PupilSizeBlinkDetector(BaseBlinkDetector):
+
+    # TODO: Implement this class
+    # see implementation in https://github.com/Yuvishap/Gaze-Project/blob/67acb26fc90e5148a05b47ca7711306c94b79ed7/Gaze/src/pre_processing/business/BlinksLogic.py
+
+    raise NotImplementedError("This class is not implemented yet")
+
+
