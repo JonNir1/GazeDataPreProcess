8eaf4200901fe4c8cb41e911ec5cb7fe8d4407eb
JonNir <jonathan.nir@mail.huji.ac.il>
2023-03-08 15:05:26 +0200

    delete unused func

--- EventDetectors/utils.py
+++ EventDetectors/utils.py
@@ -1,17 +1,6 @@
 from math import ceil
-import numpy as np
-import statistics as st
 
 import constants as c
-
-
-def calculate_sampling_rate(timestamps: np.ndarray) -> float:
-    """
-    Calculates the sampling rate of the given timestamps.
-    :param timestamps: 1D array of timestamps in microseconds
-    :return: sampling rate in Hz
-    """
-    return c.MICROSECONDS / st.mode(np.diff(timestamps))
 
 
 def calculate_minimum_sample_count(min_duration: float, sampling_rate: float) -> int:
