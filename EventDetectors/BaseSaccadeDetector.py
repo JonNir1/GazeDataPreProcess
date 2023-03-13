340e27e4b535397c2ad09326c8919e9c04a6c628
JonNir <jonathan.nir@mail.huji.ac.il>
2023-03-13 17:15:46 +0200

    implement BaseSaccadeDetector.py

--- EventDetectors/BaseSaccadeDetector.py
+++ EventDetectors/BaseSaccadeDetector.py
@@ -1,5 +1,6 @@
 from abc import ABC
 
+import experiment_config as conf
 from EventDetectors.BaseDetector import BaseDetector
 
 
@@ -7,3 +8,17 @@ class BaseSaccadeDetector(BaseDetector, ABC):
     """
     Baseclass for all saccade event detectors.
     """
+
+    def __init__(self,
+                 min_duration: float = conf.SACCADE_MINIMUM_DURATION,
+                 sr: float = conf.SAMPLING_RATE,
+                 iet: float = conf.INTER_EVENT_TIME):
+        super().__init__(sr, iet)
+        self.__min_duration = min_duration
+
+    @property
+    def min_duration(self) -> float:
+        return self.__min_duration
+
+    def set_min_duration(self, min_duration: float):
+        self.__min_duration = min_duration
