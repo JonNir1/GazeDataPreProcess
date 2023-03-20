f40cfb54496c72bc3d8659e1c95010f3d5cdcc42
JonNir <jonathan.nir@mail.huji.ac.il>
2023-03-20 12:15:44 +0200

    create BaseFixationDetector.py

---
+++ EventDetectors/BaseFixationDetector.py
@@ -0,0 +1,16 @@
+from abc import ABC
+
+import experiment_config as conf
+from EventDetectors.BaseDetector import BaseDetector
+
+
+class BaseFixationDetector(BaseDetector, ABC):
+    """
+    Baseclass for all fixation event detectors.
+    """
+
+    def __init__(self,
+                 min_duration: float = conf.FIXATION_MINIMUM_DURATION,
+                 sr: float = conf.SAMPLING_RATE,
+                 iet: float = conf.INTER_EVENT_TIME):
+        super().__init__(min_duration, sr, iet)
