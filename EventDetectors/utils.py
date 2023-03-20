c62b7931a99f7083d95125fd3dcaaf65c3a5a8ad
JonNir <jonathan.nir@mail.huji.ac.il>
2023-03-20 16:20:04 +0200

    add shift_array utility functions

--- EventDetectors/utils.py
+++ EventDetectors/utils.py
@@ -4,6 +4,23 @@ from typing import Tuple
 import experiment_config as conf
 
 
+def shift_array(array: np.ndarray, shift: int) -> np.ndarray:
+    """
+    Shifts an array by a given amount.
+        If the shift is positive, the array is shifted to the right.
+        If the shift is negative, the array is shifted to the left.
+    :param array: np.ndarray - the array to be shifted.
+    :param shift: int - the amount to shift the array by.
+    :return: shifted_array: np.ndarray - the shifted array.
+    """
+    shifted_array = np.roll(array, shift)
+    if shift > 0:
+        shifted_array[:shift] = np.nan
+    elif shift < 0:
+        shifted_array[shift:] = np.nan
+    return shifted_array
+
+
 def deg2rad(deg):
     return deg * np.pi / 180
 
