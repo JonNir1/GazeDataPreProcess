6a369767efc06c0c588a40144a34708e5aeec37d
JonNir <jonathan.nir@mail.huji.ac.il>
2023-03-26 17:46:56 +0300

    todo

---
+++ OmerDetectors/main.py
@@ -0,0 +1,6 @@
+
+# TODO: build a main controller that will:
+# 1. load the configuration file
+# 2. parse GazeData and TriggerLog files
+# 3. detect gaze events
+# 4. extract gaze events to a dataframe
