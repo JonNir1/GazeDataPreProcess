ff80bf097f02657dcf39eb635b777f0730314583
JonNir <jonathan.nir@mail.huji.ac.il>
2023-03-26 11:12:14 +0300

    remove unused const

--- experiment_config.py
+++ experiment_config.py
@@ -7,6 +7,4 @@ SCREEN_WIDTH = 53.5   # width of the screen in cm
 SCREEN_HEIGHT = 31    # height of the screen in cm
 SCREEN_RESOLUTION = (1920, 1080)  # resolution of the screen in pixels
 
-MISSING_VALUE = -1
-
 ADDITIONAL_COLUMNS = []  # additional columns to be added to the gaze data
