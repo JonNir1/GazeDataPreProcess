e5d1196277c0995075d052d7f80a6ae020836c92
JonNir <jonathan.nir@mail.huji.ac.il>
2023-05-04 14:15:01 +0300

    LWSStimInfo follows same guidelines as LWSStim

--- LWSStimuli/StimulusInfo.py
+++ LWSStimuli/StimulusInfo.py
@@ -1,22 +1,17 @@
 import os
 import numpy as np
-from enum import IntEnum
 from scipy.io import loadmat
 
-
-class ImageArrayType(IntEnum):
-    BW = 0
-    COLOR = 1
-    NOISE = 2
+from LWSStimuli.LWSStimulusTypeEnum import LWSStimulusTypeEnum, identify_stimulus_type
 
 
 class StimulusInfo:
 
-    def __init__(self, array_type, array_id: int,
+    def __init__(self, stim_id: int, stim_type,
                  image_paths: np.ndarray, image_centers: np.ndarray,
                  image_categories: np.ndarray, is_target_image: np.ndarray):
-        self.__type = self.__identify_array_type(array_type)
-        self.__id = array_id
+        self.__stim_id = stim_id
+        self.__stim_type = identify_stimulus_type(stim_type)
         self.__image_centers = image_centers
         self.__image_paths = image_paths
         self.__image_categories = image_categories
@@ -31,7 +26,7 @@ class StimulusInfo:
 
         f = loadmat(file_path)
         array_type_str = os.path.basename(os.path.dirname(file_path))
-        array_type = StimulusInfo.__identify_array_type(array_type_str)
+        array_type = identify_stimulus_type(array_type_str)
         array_id = int(os.path.basename(file_path).split('_')[1].split('.')[0])
 
         mat = f["imageInfo"]
@@ -40,37 +35,24 @@ class StimulusInfo:
         image_categories = mat["categoryInArray"][0][0].astype(int)  # shape (r, c)
         is_target_image = mat["targetsInArray"][0][0].astype(bool)    # shape (r, c)
 
-        return StimulusInfo(array_type=array_type, array_id=array_id,
+        return StimulusInfo(stim_id=array_id, stim_type=array_type,
                             image_paths=image_paths, image_centers=image_centers,
                             image_categories=image_categories, is_target_image=is_target_image)
 
     @property
-    def array_id(self) -> str:
-        return f"{self.__type.name.upper()}{self.__id}"
+    def stim_id(self) -> int:
+        return self.__stim_id
+
+    @property
+    def stim_type(self) -> LWSStimulusTypeEnum:
+        return self.__stim_type
 
     @property
     def num_targets(self) -> int:
         return np.sum(self.__is_target_image)
 
-    @staticmethod
-    def __identify_array_type(array_type) -> ImageArrayType:
-        if isinstance(array_type, ImageArrayType):
-            return array_type
-        if isinstance(array_type, str):
-            if array_type.lower() == "bw":
-                return ImageArrayType.BW
-            elif array_type.lower() == "color":
-                return ImageArrayType.COLOR
-            elif array_type.lower() == "noise":
-                return ImageArrayType.NOISE
-        if isinstance(array_type, int):
-            if array_type == 0:
-                return ImageArrayType.BW
-            elif array_type == 1:
-                return ImageArrayType.COLOR
-            elif array_type == 2:
-                return ImageArrayType.NOISE
-        raise ValueError("Invalid array type: {}".format(array_type))
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}_{self.stim_type.name.upper()}{self.stim_id}"
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}_{self.array_id}"
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}_{self.stim_type.name.upper()}{self.stim_id}"
