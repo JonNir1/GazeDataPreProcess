fb5a941e7d23bce23b82a2e371ab5779cc433306
JonNir <jonathan.nir@mail.huji.ac.il>
2023-05-04 13:05:07 +0300

    remove double imports

--- playground.py
+++ playground.py
@@ -2,18 +2,11 @@ import os
 import numpy as np
 import pandas as pd
 import scipy as sp
-import scipy as sp
 import matplotlib.pyplot as plt
 import cv2
 
 import constants as cnst
 import experiment_config as cnfg
-
-from LWSStimuli.ImageArray import ImageArray
-
-ia1 = ImageArray.from_file(r"S:\Lab-Shared\Experiments\LWS Free Viewing Demo\Stimuli\generated_stim1\bw\image_1.mat")
-
-
 
 from LWSStimuli.ImageArray import ImageArray
 
@@ -41,3 +34,24 @@ t2_with_events = pd.concat([t2, pd.DataFrame({'is_blink': is_blink, 'is_saccade'
 fe = extract_events_to_dataframe(event_type='fixation', timestamps=t2[cnst.MICROSECONDS].values / 1000,
                                  is_event=is_fixation, sampling_rate=sr,
                                  x=t2[cnst.LEFT_X].values, y=t2[cnst.LEFT_Y].values)
+
+
+
+# create a video of eye movements
+screen_w, screen_h = cnfg.SCREEN_RESOLUTION
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+video = cv2.VideoWriter('video.mp4', fourcc, round(sr), (screen_w, screen_h))
+
+for i, row in t2_with_events.iterrows():
+    x = row[cnst.LEFT_X]
+    y = row[cnst.LEFT_Y]
+
+    # create a blank image and add circle around the gaze point
+    img = np.zeros((screen_h, screen_w, 3), dtype=np.uint8)
+    if not pd.isna(x) and not pd.isna(y):
+        cv2.circle(img, (round(x), round(y)),
+                   10, (0, 0, 255), -1)
+    video.write(img)
+
+# release the video
+video.release()
