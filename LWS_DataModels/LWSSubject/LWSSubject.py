8e51f1095362f0f2e2a5b765e2235a4c8da8ed02
JonNir <jonathan.nir@mail.huji.ac.il>
2023-05-08 16:53:16 +0300

    typo

--- LWS_DataModels/LWSSubject/LWSSubject.py
+++ LWS_DataModels/LWSSubject/LWSSubject.py
@@ -38,7 +38,7 @@ class LWSSubject:
         if len(gaze_paths) > 1:
             sr, tobii_trials = self.__handle_multiple_sessions()
         else:
-            sr, tobii_trials = parse_tobii_gaze_and_triggers(gaze_path, triggers_path)
+            sr, tobii_trials = parse_tobii_gaze_and_triggers(gaze_paths, triggers_paths)
         self.__sampling_rate = sr
         self.__tobii_trials = tobii_trials
 
