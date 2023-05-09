e32b5a7c456d0e02cf5aa72e1a212d988c112b7d
JonNir <jonathan.nir@mail.huji.ac.il>
2023-05-09 15:17:09 +0300

    refactor: union enum files

--- LWS_DataModels/LWSEnums.py
+++ LWS_DataModels/LWSEnums.py
@@ -5,3 +5,19 @@ class LWSStimulusTypeEnum(StrEnum):
     BW = 'bw'
     COLOR = 'color'
     NOISE = 'noise'
+
+
+class LWSSubjectSexEnum(StrEnum):
+    Male = 'Male'
+    Female = 'Female'
+    Other = 'Other'
+
+
+class LWSSubjectDominantHandEnum(StrEnum):
+    Right = 'Right'
+    Left = 'Left'
+
+
+class LWSSubjectDominantEyeEnum(StrEnum):
+    Right = 'Right'
+    Left = 'Left'
