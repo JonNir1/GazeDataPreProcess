243f44bdbdc141153feeaad6462d274cf41548e7
JonNir <jonathan.nir@mail.huji.ac.il>
2023-05-11 14:48:03 +0300

    maintain `is_processed` state

--- LWS/DataModels/LWSTrial.py
+++ LWS/DataModels/LWSTrial.py
@@ -17,6 +17,7 @@ class LWSTrial:
                  subject_info: LWSSubjectInfo,
                  stimulus: LWSArrayStimulus,
                  behavioral_data: LWSBehavioralData):
+        self.__is_processed = False
         self.__trial_num = trial_num
         self.__subject_info = subject_info
         self.__stimulus = stimulus
@@ -35,8 +36,22 @@ class LWSTrial:
         return self.__stimulus
 
     @property
+    def is_processed(self) -> bool:
+        return self.__is_processed
+
+    def set_is_processed(self, is_processed: bool):
+        if self.__is_processed and not is_processed:
+            raise RuntimeError("Cannot set is_processed to False after it has been set to True.")
+        self.__is_processed = is_processed
+
+    @property
     def behavioral_data(self) -> LWSBehavioralData:
         return self.__behavioral_data
+
+    def set_behavioral_data(self, behavioral_data: LWSBehavioralData):
+        if self.is_processed:
+            raise RuntimeError("Cannot set behavioral data after trial has been processed.")
+        self.__behavioral_data = behavioral_data
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}_S{self.__subject_info.subject_id}_T{self.__trial_num}"
