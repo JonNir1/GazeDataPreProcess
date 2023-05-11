de4be597bf4059771e5d42541944ec9e4d1e6be9
JonNir <jonathan.nir@mail.huji.ac.il>
2023-05-11 17:46:00 +0300

    update

--- LWS/scripts/process_subject.py
+++ LWS/scripts/process_subject.py
@@ -1,9 +1,21 @@
+import os
 from typing import Optional
 
 import experiment_config as cnfg
 from Utils.ScreenMonitor import ScreenMonitor
+from LWS.scripts.read_subject import read_subject
+from LWS.scripts.detect_events import detect_all_events
 
 
 def process_subject(subject_dir: str, stimuli_dir: str = cnfg.STIMULI_DIR,
-                    screen_monitor: Optional[ScreenMonitor] = None):
+                    screen_monitor: Optional[ScreenMonitor] = None, **kwargs):
+    if not os.path.isdir(subject_dir):
+        raise NotADirectoryError(f"Directory {subject_dir} does not exist.")
+    if not os.path.isdir(stimuli_dir):
+        raise NotADirectoryError(f"Directory {stimuli_dir} does not exist.")
+    screen_monitor = screen_monitor if screen_monitor is not None else ScreenMonitor.from_config()
+    sr, trials = read_subject(subject_dir, stimuli_dir, screen_monitor)
+    for i, trial in enumerate(trials):
+        is_blink, is_saccade, is_fixation = detect_all_events(trial, sr, **kwargs)
+
     pass
--- playground.py
+++ playground.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import pandas as pd
 import cv2
@@ -5,9 +6,13 @@ import cv2
 import constants as cnst
 import experiment_config as cnfg
 
+from Utils.ScreenMonitor import ScreenMonitor
 from LWS.scripts.read_subject import read_subject
 from LWS.scripts.detect_events import detect_all_events
-from Utils.ScreenMonitor import ScreenMonitor
+from LWS.scripts.extract_events import extract_all_events, extract_event
+
+
+start = time.time()
 
 sm = ScreenMonitor.from_config()
 sr, trials = read_subject(subject_dir=r"S:\Lab-Shared\Experiments\LWS Free Viewing Demo\RawData\Rotem Demo",
@@ -17,20 +22,26 @@ is_blink, is_saccade, is_fixation = detect_all_events(trial=trial1, sampling_rat
                                                       stuff_with='fixation',
                                                       blink_detector_type='missing data',
                                                       saccade_detector_type='engbert')
+trial1.behavioral_data.concat(pd.DataFrame({'is_blink': is_blink, 'is_saccade': is_saccade, 'is_fixation': is_fixation}))
+# t_df = trial1.behavioral_data._LWSBehavioralData__data
+
+fix_events_list = extract_event(trial=trial1, event_type='fixation', sampling_rate=sr)
+all_events_list = extract_all_events(trial=trial1, sampling_rate=sr)
 
 
+end = time.time()
+print(f"Total time: {end - start}")
 
-from GazeEvents.scripts.extract_gaze_events import extract_events_to_dataframe
+##########################################
 
+from GazeEvents.scripts.extract_gaze_events import extract_events_to_list
 
-t2_with_events = pd.concat([t2, pd.DataFrame({'is_blink': is_blink, 'is_saccade': is_saccade,
-                                              'is_fixation': is_fixation})], axis=1)
-
-fe = extract_events_to_dataframe(event_type='fixation', timestamps=t2[cnst.MICROSECONDS].values / 1000,
+ts, x, y = trial1.get_raw_gaze_coordinates()
+fe_list = extract_events_to_list(event_type='fixation', timestamps=ts,
                                  is_event=is_fixation, sampling_rate=sr,
-                                 x=t2[cnst.LEFT_X].values, y=t2[cnst.LEFT_Y].values)
+                                 x=x, y=y)
 
-
+##########################################
 
 # create a video of eye movements
 screen_w, screen_h = cnfg.SCREEN_RESOLUTION
