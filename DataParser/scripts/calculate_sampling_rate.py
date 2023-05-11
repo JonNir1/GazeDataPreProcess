9f2f1887159becb176afff50c7086e849a5b224d
JonNir <jonathan.nir@mail.huji.ac.il>
2023-05-11 13:01:15 +0300

    delete unused file

--- DataParser/scripts/calculate_sampling_rate.py
+++
@@ -1,8 +0,0 @@
-from DataParser.TobiiGazeDataParser import TobiiGazeDataParser
-
-
-def calculate_sampling_rate_for_tobii(tobii_path) -> float:
-    # Returns the actual sampling rate of the Tobii data.
-    tobii_parser = TobiiGazeDataParser(tobii_path)
-    return tobii_parser.sampling_rate
-
