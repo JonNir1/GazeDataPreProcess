ece93d818f6b8745ea5d6aa602d5fe17a218f256
JonNir <jonathan.nir@mail.huji.ac.il>
2023-05-11 14:21:16 +0300

    create process_subject.py

---
+++ LWS/scripts/process_subject.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+import experiment_config as cnfg
+from Utils.ScreenMonitor import ScreenMonitor
+
+
+def process_subject(subject_dir: str, stimuli_dir: str = cnfg.STIMULI_DIR,
+                    screen_monitor: Optional[ScreenMonitor] = None):
+    pass
