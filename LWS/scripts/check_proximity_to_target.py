534bac5b95fc7288f049a1887e775d26dfc4924b
JonNir <jonathan.nir@mail.huji.ac.il>
2023-05-14 13:07:06 +0300

    naming

--- LWS/scripts/check_proximity_to_target.py
+++ LWS/scripts/check_proximity_to_target.py
@@ -4,13 +4,13 @@ from Utils.ScreenMonitor import ScreenMonitor
 
 
 def check_proximity_to_target(tx: float, ty: float,
-                              x: np.ndarray, y: np.ndarray,
+                              xs: np.ndarray, ys: np.ndarray,
                               d: float, angle: float,
                               sm: ScreenMonitor) -> np.ndarray:
     """
     Check if the gaze point is within a certain distance from the target.
     :param tx, ty: target x, y coordinates
-    :param x, y: arrays of gaze x, y coordinates
+    :param xs, ys: arrays of gaze x, y coordinates
     :param d: distance of the viewer from the screen
     :param angle: visual angle considered proximal to the target
     :param sm: ScreenMonitor object
@@ -21,16 +21,16 @@ def check_proximity_to_target(tx: float, ty: float,
     :raises ValueError: if x and y are not of the same length, if any of the given coordinates is NaN, or if d or angle
         are NaN.
     """
-    if len(x) != len(y):
+    if len(xs) != len(ys):
         raise ValueError("x and y must be of the same length")
     if np.isnan(tx) or np.isnan(ty):
         raise ValueError("Target coordinates cannot be NaN")
     if np.isnan(d) or np.isnan(angle):
         raise ValueError("Distance and angle cannot be NaN")
 
-    is_close = np.zeros(len(x), dtype=bool)
-    for i in range(len(x)):
-        angle_between = sm.calc_angle_between_pixels(d, p1=(tx, ty), p2=(x[i], y[i]), use_radians=False)
+    is_close = np.zeros(len(xs), dtype=bool)
+    for i in range(len(xs)):
+        angle_between = sm.calc_angle_between_pixels(d, p1=(tx, ty), p2=(xs[i], ys[i]), use_radians=False)
         if np.isnan(angle_between):
             is_close[i] = False
         else:
