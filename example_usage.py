2a351c9a968affd257ce9f05161402c8102dd02f
JonNir <jonathan.nir@mail.huji.ac.il>
2023-05-21 13:29:44 +0300

    refactor data parsers: update example_usage.py [12/n]

--- example_usage.py
+++ example_usage.py
@@ -7,8 +7,9 @@ import pandas as pd
 import constants as cnst
 import experiment_config as cnfg
 
-from DataParser.TobiiGazeDataParser import TobiiGazeDataParser
 from Utils.ScreenMonitor import ScreenMonitor
+from Utils.calculate_sampling_rate import calculate_sampling_rate_from_microseconds
+from DataParser.scripts.parse_eye_tracker import parse_eye_tracker
 from EventDetectors.scripts.detect_events import detect_all_events
 from GazeEvents.scripts.extract_gaze_events import extract_events_to_dataframe
 
@@ -16,13 +17,10 @@ from GazeEvents.scripts.extract_gaze_events import extract_events_to_dataframe
 sm = ScreenMonitor.from_config()
 
 # parse eye tracking data
-path = r"Path/To/Your/Data"
-parser = TobiiGazeDataParser(input_path=path, screen_monitor=sm)
-sr = parser.sampling_rate
-et_trials = parser.parse_and_split()  # returns a list of pd.DataFrames, one for each trial
+et_data = parse_eye_tracker(et_type="tobii", et_path=r"Path/To/Your/Data", screen_monitor=sm, split_trials=False)
 
 # classify samples as blinks, saccades or fixations
-tr = et_trials[0]
+tr = et_data[0]
 microseconds = tr[cnst.MICROSECONDS]
 x, y = tr[cnst.LEFT_X], tr[cnst.LEFT_Y]
 is_blink, is_saccade, is_fixation = detect_all_events(x, y, sampling_rate=sr,
@@ -31,6 +29,7 @@ is_blink, is_saccade, is_fixation = detect_all_events(x, y, sampling_rate=sr,
                                                       stuff_with="fixation")
 
 # extract events to a pandas DataFrame
+sr = calculate_sampling_rate_from_microseconds(microseconds)
 blinks_summary = extract_events_to_dataframe(event_type="blink", timestamps=microseconds, is_event=is_blink,
                                              sampling_rate=sr)
 saccades_summary = extract_events_to_dataframe(event_type="saccade", timestamps=microseconds, is_event=is_saccade,
