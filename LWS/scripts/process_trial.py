058d654219ecf6de48873700fadc5f0922402be0
JonNir <jonathan.nir@mail.huji.ac.il>
2023-05-21 14:52:59 +0300

    add docstring

--- LWS/scripts/process_trial.py
+++ LWS/scripts/process_trial.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-import experiment_config as cnfg
 from Utils.ScreenMonitor import ScreenMonitor
 from LWS.DataModels.LWSTrial import LWSTrial
 from LWS.scripts.distance_to_targets import calculate_angular_distance_for_gaze_data
@@ -8,32 +7,50 @@ from LWS.scripts.detect_events import detect_all_events
 from LWS.scripts.extract_events import extract_all_events
 
 
-def process_trial(trial: LWSTrial, sr: float, screen_monitor: ScreenMonitor = None, **kwargs):
+def process_trial(trial: LWSTrial, **kwargs):
     """
     Processes the given trial and adds the processed data to the trial object.
 
-    TODO: add docstring
+    keyword arguments:
+        - screen_monitor: The screen monitor used to display the stimuli.
 
-    :param trial:
-    :param sr:
-    :param screen_monitor:
-    :param kwargs:
-    :return:
+    gaze detection kwargs:
+        - stuff_with: either "saccade", "fixation" or None. Controls how to fill unidentified samples.
+        - blink_detector_type: The type of blink detector to use. If None, no blink detection is performed.
+        - saccade_detector_type: The type of saccade detector to use. If None, no saccade detection is performed.
+        - fixation_detector_type: The type of fixation detector to use. If None, no fixation detection is performed.
+        - drop_outlier_events: If True, drops events that are considered outliers. If False, keeps all events.
+
+    blink kwargs:
+        - blink_inter_event_time: minimal time between two events in ms;                                default: 5 ms
+        - blink_min_duration: minimal duration of a blink in ms;                                        default: 50 ms
+        - missing_value: default value indicating missing data, used by MissingDataBlinkDetector;       default: np.nan
+
+    saccade kwargs:
+        - saccade_inter_event_time: minimal time between two events in ms;                              default: 5 ms
+        - saccade_min_duration: minimal duration of a blink in ms;                                      default: 5 ms
+        - derivation_window_size: window size for derivation in ms;                                     default: 3 ms
+        - lambda_noise_threshold: threshold for lambda noise, used by EngbertSaccadeDetector;           default: 5
+
+    fixation kwargs:
+        - fixation_inter_event_time: minimal time between two events in ms;                             default: 5 ms
+        - fixation_min_duration: minimal duration of a blink in ms;                                     default: 55 ms
+        - velocity_threshold: maximal velocity allowed within a fixation, used by IVTFixationDetector;  default: 30 deg/s
+
     """
-    screen_monitor = screen_monitor if screen_monitor is not None else ScreenMonitor.from_config()
-
     trial.is_processed = False
+    sm = kwargs.pop('screen_monitor', None) or ScreenMonitor.from_config()
 
     # process raw eye-tracking data
-    is_blink, is_saccade, is_fixation = detect_all_events(trial, sr, **kwargs)
-    target_distance = calculate_angular_distance_for_gaze_data(trial, screen_monitor)
+    is_blink, is_saccade, is_fixation = detect_all_events(trial, **kwargs)
+    target_distance = calculate_angular_distance_for_gaze_data(trial, sm=sm)
     is_event_df = pd.DataFrame({'is_blink': is_blink, 'is_saccade': is_saccade,
                                 'is_fixation': is_fixation, 'target_distance': target_distance})
     trial.get_behavioral_data().concat(is_event_df, deep_copy=False)  # add the new columns to the behavioral data
 
     # process gaze events
     drop_outlier_events = kwargs.pop('drop_outlier_events', False)
-    events = extract_all_events(trial, screen_monitor, drop_outliers=drop_outlier_events)
+    events = extract_all_events(trial, sm, drop_outliers=drop_outlier_events)
     trial.set_gaze_events(events)
 
     trial.is_processed = True
