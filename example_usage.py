c877bc5cb955935a7ab9fb8c778a2481150b76b6
JonNir <jonathan.nir@mail.huji.ac.il>
2023-05-23 15:37:12 +0300

    update

--- example_usage.py
+++ example_usage.py
@@ -21,17 +21,19 @@ et_data = parse_eye_tracker(et_type="tobii", et_path=r"Path/To/Your/Data",
                             screen_monitor=sm, split_trials=False,
                             additional_columns=cnfg.ADDITIONAL_COLUMNS)
 
-# classify samples as blinks, saccades or fixations
+# extract data from the first trial
 tr = et_data[0]
-microseconds = tr[cnst.MICROSECONDS]
-x, y = tr[cnst.LEFT_X], tr[cnst.LEFT_Y]
+microseconds = tr[cnst.MICROSECONDS].values
+x, y = tr[cnst.LEFT_X].values, tr[cnst.LEFT_Y].values
+sr = calculate_sampling_rate_from_microseconds(microseconds)
+
+# classify samples as blinks, saccades or fixations
 is_blink, is_saccade, is_fixation = detect_all_events(x, y, sampling_rate=sr,
                                                       blink_detector_type="missing_data",
                                                       saccade_detector_type="engbert",
                                                       stuff_with="fixation")
 
 # extract events to a pandas DataFrame
-sr = calculate_sampling_rate_from_microseconds(microseconds)
 blinks_summary = extract_events_to_dataframe(event_type="blink", timestamps=microseconds, is_event=is_blink,
                                              sampling_rate=sr)
 saccades_summary = extract_events_to_dataframe(event_type="saccade", timestamps=microseconds, is_event=is_saccade,
