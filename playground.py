5b94c1c40643d1a8492a29ec7f6b140ae3d947f5
JonNir <jonathan.nir@mail.huji.ac.il>
2023-05-29 13:24:22 +0300

    clean playground.py

--- playground.py
+++ playground.py
@@ -1,3 +1,4 @@
+import os
 import time
 import numpy as np
 import pandas as pd
@@ -5,15 +6,15 @@ import cv2
 
 import constants as cnst
 import experiment_config as cnfg
-
 from Utils.ScreenMonitor import ScreenMonitor
 import LWS.PreProcessing as pp
 
-
 start = time.time()
 
+subject_dir = r"S:\Lab-Shared\Experiments\LWS Free Viewing Demo\RawData\Rotem Demo"
 sm = ScreenMonitor.from_config()
-trials = pp.process_subject(subject_dir=r"S:\Lab-Shared\Experiments\LWS Free Viewing Demo\RawData\Rotem Demo",
+
+trials = pp.process_subject(subject_dir=subject_dir,
                             stimuli_dir=cnfg.STIMULI_DIR,
                             screen_monitor=sm,
                             stuff_with='fixation',
@@ -21,27 +22,10 @@ trials = pp.process_subject(subject_dir=r"S:\Lab-Shared\Experiments\LWS Free Vie
                             saccade_detector_type='engbert',
                             drop_outlier_events=False)
 
-trial1 = trials[0]
+# trial1 = trials[0]
 
 end = time.time()
 print(f"Total time: {end - start}")
-
-##########################################
-
-for i, tr in enumerate(trials):
-    ge = tr.get_gaze_events()
-    fixs = list(filter(lambda e: e.event_type() == cnst.FIXATION, ge))
-    centers = [f.center_of_mass for f in fixs]
-    has_nans = any([np.isnan(c).any() for c in centers])
-    if has_nans:
-        print(f"Trial {i} has nans")
-        break
-
-tr22_data = tr._LWSTrial__behavioral_data._LWSBehavioralData__data
-tr22_data[tr22_data["is_fixation"]]['right_y'].isna().sum()
-
-# TODO: check why there are nans in the data
-# TODO: use binocular data instead of monocular?
 
 ##########################################
 
