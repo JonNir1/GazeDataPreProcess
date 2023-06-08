4581e86081acc9954fb565db6eb87ff8ca1c61cd
JonNir <jonathan.nir@mail.huji.ac.il>
2023-06-08 12:27:46 +0300

    update

--- playground.py
+++ playground.py
@@ -8,6 +8,7 @@ import constants as cnst
 import experiment_config as cnfg
 from Utils.ScreenMonitor import ScreenMonitor
 import LWS.PreProcessing as pp
+from LWS.DataModels.LWSVisualizer import LWSVisualizer
 
 start = time.time()
 
@@ -21,11 +22,33 @@ trials = pp.process_subject(subject_dir=os.path.join(cnfg.RAW_DATA_DIR, 'Rotem D
                             saccade_detector_type='engbert',
                             drop_outlier_events=False)
 
-trial11 = trials[10]
-trial11_raw_data = trial11._LWSTrial__behavioral_data._LWSBehavioralData__data
-trial11_fixations = trial11.get_gaze_events(event_type=cnst.FIXATION)
-trial11_fix1 = trial11_fixations[0]
+end = time.time()
+print(f"Finished preprocessing in: {(end - start):.2f} seconds")
+
+
+start = time.time()
+
+visualizer = LWSVisualizer(screen_monitor=sm)
+
+for i, tr in enumerate(trials):
+    start_trial = time.time()
+    visualizer.visualize(trial=tr, output_directory=cnfg.OUTPUT_DIR, show=False)
+    end_trial = time.time()
+    print(f"\tTrial {i}:\t{(end_trial - start_trial):.2f} s")
 
 end = time.time()
-pp_duration = end - start
-print(f"Finished preprocessing in: {pp_duration} seconds")
+print(f"Finished visualization in: {(end - start):.2f} seconds")
+
+##########################################
+# keep only relevant variables:
+
+del start
+del end
+del i
+del tr
+
+# trial11 = trials[10]
+# trial11_raw_data = trial11._LWSTrial__behavioral_data._LWSBehavioralData__data
+# trial11_fixations = trial11.get_gaze_events(event_type=cnst.FIXATION)
+# trial11_fix1 = trial11_fixations[0]
+
