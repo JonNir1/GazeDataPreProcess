de9d06f8e0aaaed9887b7751c8fee1356f8dd51a
JonNir <jonathan.nir@mail.huji.ac.il>
2023-06-14 16:35:12 +0300

    change the way we calculate sampling rate

--- Utils/calculate_sampling_rate.py
+++ Utils/calculate_sampling_rate.py
@@ -8,12 +8,12 @@ import constants as cnst
 def calculate_sampling_rate_from_milliseconds(milliseconds: Union[np.ndarray, pd.Series]) -> float:
     if isinstance(milliseconds, np.ndarray):
         milliseconds = pd.Series(milliseconds)
-    return cnst.MILLISECONDS_PER_SECOND / milliseconds.diff().mode()[0]
+    return cnst.MILLISECONDS_PER_SECOND / milliseconds.diff().mean()
 
 
 def calculate_sampling_rate_from_microseconds(microseconds: Union[np.ndarray, pd.Series]) -> float:
     if isinstance(microseconds, np.ndarray):
         microseconds = pd.Series(microseconds)
-    return cnst.MICROSECONDS_PER_SECOND / microseconds.diff().mode()[0]
+    return cnst.MICROSECONDS_PER_SECOND / microseconds.diff().mean()
 
 
