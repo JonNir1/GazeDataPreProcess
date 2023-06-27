15b8e3779abaf3298b0e97bb468ec95760962c01
JonNir <jonathan.nir@mail.huji.ac.il>
2023-06-27 13:55:53 +0300

    increase fixation std

--- LWS/analysis_scripts/heatmaps.py
+++ LWS/analysis_scripts/heatmaps.py
@@ -31,19 +31,21 @@ def pixel_counts(trial: LWSTrial) -> np.ndarray:
     return counts
 
 
-def gaze_heatmap(trial: LWSTrial, smoothing_sigma: float) -> np.ndarray:
+def gaze_heatmap(trial: LWSTrial, smoothing_std: float) -> np.ndarray:
     """
     Returns a 2D array with the same shape as the stimulus image, where each entry is the relative amount of samples
     that fell on the corresponding pixel.
+    :param trial: the trial to analyze
+    :param smoothing_std: the standard deviation of the Gaussian used to smooth the heatmap
 
     :raises ValueError: if `smoothing_sigma` is nan, inf, zero or negative.
     """
     heatmap = pixel_counts(trial)
-    if (smoothing_sigma is None) or (not np.isfinite(smoothing_sigma)):
-        raise ValueError(f"argument `smoothing_sigma` must be a finite number, got {smoothing_sigma}")
-    if smoothing_sigma <= 0:
-        raise ValueError(f"argument `smoothing_sigma` must be positive, got {smoothing_sigma}")
-    heatmap = gaussian_filter(heatmap, sigma=smoothing_sigma)
+    if (smoothing_std is None) or (not np.isfinite(smoothing_std)):
+        raise ValueError(f"argument `smoothing_std` must be a finite number, got {smoothing_std}")
+    if smoothing_std <= 0:
+        raise ValueError(f"argument `smoothing_sigma` must be positive, got {smoothing_std}")
+    heatmap = gaussian_filter(heatmap, sigma=smoothing_std)
     # normalize heatmap to values in [0, 1]
     heatmap = au.normalize_array(heatmap)
     return heatmap
@@ -54,7 +56,7 @@ def fixations_heatmap(trial: LWSTrial) -> np.ndarray:
     Returns a 2D array with the same shape as the stimulus image, where each pixel is colored according to the total
     duration of all fixations that fell on it. Each fixation adds a 2D Gaussian to the heatmap, with the center of the
     Gaussian at the fixation's center of mass, it's mean equal to the fixation's duration (in milliseconds), and the
-    standard deviation of the Gaussian equal to the fixation's standard deviation (on the X and Y axes).
+    standard deviation of the Gaussian equal to double the fixation's standard deviation (on the X and Y axes).
     """
     h, w = trial.get_stimulus().image_shape
     heatmap = np.zeros((h, w))
@@ -67,7 +69,7 @@ def fixations_heatmap(trial: LWSTrial) -> np.ndarray:
 
         gaus = np.zeros_like(heatmap)
         gaus[round(center_y), round(center_x)] = duration
-        gaus = gaussian_filter(gaus, sigma=[std_y, std_x])
+        gaus = gaussian_filter(gaus, sigma=[2 * std_y, 2 * std_x])
         heatmap += gaus
     # normalize heatmap to values in [0, 1]
     heatmap = au.normalize_array(heatmap)
