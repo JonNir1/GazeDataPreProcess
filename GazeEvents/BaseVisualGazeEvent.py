8d8eb4f456e70df2e95a3314abb6b7f5057791b1
JonNir <jonathan.nir@mail.huji.ac.il>
2023-06-28 18:19:19 +0300

    add velocity calculations

--- GazeEvents/BaseVisualGazeEvent.py
+++ GazeEvents/BaseVisualGazeEvent.py
@@ -1,5 +1,6 @@
 from abc import ABC
 import numpy as np
+import pandas as pd
 
 from GazeEvents.BaseGazeEvent import BaseGazeEvent
 
@@ -16,9 +17,38 @@ class BaseVisualGazeEvent(BaseGazeEvent, ABC):
         if len(timestamps) != len(x) or len(timestamps) != len(y):
             raise ValueError("Arrays of timestamps, x and y must have the same length")
         super().__init__(timestamps=timestamps)
+        self._viewer_distance = viewer_distance  # in cm
         self._x = x
         self._y = y
-        self._viewer_distance = viewer_distance  # in cm
+        self.velocities = self.__calculate_velocities()
+
+    @property
+    def max_velocity(self) -> float:
+        """ Returns the maximum velocity of the event in pixels per second """
+        return float(np.nanmax(self.velocities)) * 1000
+
+    @property
+    def mean_velocity(self) -> float:
+        """ Returns the mean velocity of the event in pixels per second """
+        return float(np.nanmean(self.velocities)) * 1000
+
+    def to_series(self) -> pd.Series:
+        """
+        creates a pandas Series with summary of saccade information.
+        :return: a pd.Series with the same values as super().to_series() and the following additional values:
+            - max_velocity: the maximum velocity of the event in pixels per second
+            - mean_velocity: the mean velocity of the event in pixels per second
+        """
+        series = super().to_series()
+        series["max_velocity"] = self.max_velocity
+        series["mean_velocity"] = self.mean_velocity
+        return series
+
+    def __calculate_velocities(self) -> np.ndarray:
+        dx = np.diff(self._x)
+        dy = np.diff(self._y)
+        dt = np.diff(self._timestamps)
+        return np.sqrt(dx ** 2 + dy ** 2) / dt
 
     def __eq__(self, other):
         if not isinstance(other, type(self)):
