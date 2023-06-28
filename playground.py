f564c4cd2813ff7748b9dc45b021b08407826320
JonNir <jonathan.nir@mail.huji.ac.il>
2023-06-28 18:22:18 +0300

    update

--- playground.py
+++ playground.py
@@ -11,27 +11,27 @@ import constants as cnst
 from Config import experiment_config as cnfg
 from LWS.DataModels.LWSSubject import LWSSubject
 
-# ##########################################
-# ### PREPROCESSING GAZE DATA  #############
-# ##########################################
-#
-# import LWS.PreProcessing as pp
-#
-# start = time.time()
-#
-# subject = pp.process_subject(subject_dir=os.path.join(cnfg.RAW_DATA_DIR, 'Rotem Demo'),
-#                              screen_monitor=cnfg.SCREEN_MONITOR,
-#                              save_pickle=True,
-#                              stuff_with='fixation',
-#                              blink_detector_type='missing data',
-#                              saccade_detector_type='engbert',
-#                              drop_outlier_events=False)
-#
-# end = time.time()
-# print(f"Finished preprocessing in: {(end - start):.2f} seconds")
-#
-# # delete irrelevant variables:
-# del start, end
+##########################################
+### PREPROCESSING GAZE DATA  #############
+##########################################
+
+import LWS.PreProcessing as pp
+
+start = time.time()
+
+subject = pp.process_subject(subject_dir=os.path.join(cnfg.RAW_DATA_DIR, 'Rotem Demo'),
+                             screen_monitor=cnfg.SCREEN_MONITOR,
+                             save_pickle=True,
+                             stuff_with='fixation',
+                             blink_detector_type='missing data',
+                             saccade_detector_type='engbert',
+                             drop_outlier_events=False)
+
+end = time.time()
+print(f"Finished preprocessing in: {(end - start):.2f} seconds")
+
+# delete irrelevant variables:
+del start, end
 
 ##########################################
 ###  LOAD DATA FROM PICKLE FILES  ########
@@ -61,11 +61,11 @@ for tr in subject.get_all_trials():
         break
     try:
         start_trial = time.time()
-        # visualizer.create_gaze_figure(trial=tr, savefig=True)
-        # visualizer.create_targets_figure(trial=tr, savefig=True)
+        visualizer.create_gaze_figure(trial=tr, savefig=True)
+        visualizer.create_targets_figure(trial=tr, savefig=True)
         visualizer.create_heatmap(trial=tr, savefig=True, fixation_only=True, show_targets_color=(0, 0, 0))
-        # visualizer.create_heatmap(trial=tr, savefig=True, fixation_only=False, show_targets_color=(0, 0, 0))
-        # visualizer.create_video(trial=tr, output_directory=cnfg.OUTPUT_DIR)
+        visualizer.create_heatmap(trial=tr, savefig=True, fixation_only=False, show_targets_color=(0, 0, 0))
+        visualizer.create_video(trial=tr, output_directory=cnfg.OUTPUT_DIR)
         end_trial = time.time()
         print(f"\t{tr.__repr__()}:\t{(end_trial - start_trial):.2f} s")
     except Exception as e:
