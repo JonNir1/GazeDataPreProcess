5b0fdba85071b73c1505f5bbcbde462c973595b2
JonNir <jonathan.nir@mail.huji.ac.il>
2023-06-28 16:14:26 +0300

    nit

--- LWS/DataModels/LWSTrialVisualizer.py
+++ LWS/DataModels/LWSTrialVisualizer.py
@@ -14,7 +14,8 @@ import LWS.analysis_scripts.heatmaps as hm
 
 
 class LWSTrialVisualizer:
-    # TODO: split to separate Visualizer classes for each type of visualization (e.g. GazeVisualizer, TargetsVisualizer, etc.)
+    # TODO: split to separate Visualizer classes for each type of visualization
+    #  (e.g. GazeVisualizer, TargetsVisualizer, etc.)
     # TODO: velocity (and acceleration?) figure with events and triggers
 
     IMAGE_SUFFIX = 'png'
