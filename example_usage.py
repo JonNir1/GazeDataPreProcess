12d5a28d5aae86eb58233e31cc05c19bedea8b40
JonNir <jonathan.nir@mail.huji.ac.il>
2023-06-28 12:33:24 +0300

    create a global variable in config file and use them globally [4/n]

--- example_usage.py
+++ example_usage.py
@@ -11,14 +11,12 @@ from Utils.ScreenMonitor import ScreenMonitor
 from Utils.calculate_sampling_rate import calculate_sampling_rate_from_microseconds
 from DataParser.scripts.parse_eye_tracker import parse_eye_tracker
 from EventDetectors.scripts.detect_events import detect_all_events
-from GazeEvents.scripts.gen_gaze_events import gen_gaze_events_summary
+from GazeEvents.BaseGazeEvent import BaseGazeEvent
+from GazeEvents.scripts.create_gaze_events import gen_gaze_events_summary
 
-# create a screen monitor object
-sm = ScreenMonitor.from_config()
 
 # parse eye tracking data
-et_data = parse_eye_tracker(et_type="tobii", et_path=r"Path/To/Your/Data",
-                            screen_monitor=sm, split_trials=False,
+et_data = parse_eye_tracker(et_type="tobii", et_path=r"Path/To/Your/Data", split_trials=False,
                             additional_columns=cnfg.ADDITIONAL_COLUMNS)
 
 # extract data from the first trial
@@ -34,7 +32,6 @@ is_blink, is_saccade, is_fixation = detect_all_events(x=np.vstack((x_l, x_r)), y
                                                       blink_detector_type="missing_data",
                                                       saccade_detector_type="engbert")
 
-# extract events to a pandas DataFrame
 blinks_summary = gen_gaze_events_summary(event_type="blink", timestamps=microseconds, is_event=is_blink,)
 saccades_summary = gen_gaze_events_summary(event_type="saccade", timestamps=microseconds, is_event=is_saccade, x=x_l, y=y_l)
 fixations_summary = gen_gaze_events_summary(event_type="fixation", timestamps=microseconds, is_event=is_fixation, x=x_l, y=y_l)
