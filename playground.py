5f61e0c13e43faa5eb045f3483e35eebd4bc8955
JonNir <jonathan.nir@mail.huji.ac.il>
2023-06-29 16:19:45 +0300

    set 0° to the East

--- playground.py
+++ playground.py
@@ -10,28 +10,29 @@ import cv2
 import constants as cnst
 from Config import experiment_config as cnfg
 from LWS.DataModels.LWSSubject import LWSSubject
+import Visualization.visualization_utils as visutils
 
-##########################################
-### PREPROCESSING GAZE DATA  #############
-##########################################
-
-import LWS.PreProcessing as pp
-
-start = time.time()
-
-subject = pp.process_subject(subject_dir=os.path.join(cnfg.RAW_DATA_DIR, 'Rotem Demo'),
-                             screen_monitor=cnfg.SCREEN_MONITOR,
-                             save_pickle=True,
-                             stuff_with='fixation',
-                             blink_detector_type='missing data',
-                             saccade_detector_type='engbert',
-                             drop_outlier_events=False)
-
-end = time.time()
-print(f"Finished preprocessing in: {(end - start):.2f} seconds")
-
-# delete irrelevant variables:
-del start, end
+# ##########################################
+# ### PREPROCESSING GAZE DATA  #############
+# ##########################################
+#
+# import LWS.PreProcessing as pp
+#
+# start = time.time()
+#
+# subject = pp.process_subject(subject_dir=os.path.join(cnfg.RAW_DATA_DIR, 'Rotem Demo'),
+#                              screen_monitor=cnfg.SCREEN_MONITOR,
+#                              save_pickle=True,
+#                              stuff_with='fixation',
+#                              blink_detector_type='missing data',
+#                              saccade_detector_type='engbert',
+#                              drop_outlier_events=False)
+#
+# end = time.time()
+# print(f"Finished preprocessing in: {(end - start):.2f} seconds")
+#
+# # delete irrelevant variables:
+# del start, end
 
 ##########################################
 ###  LOAD DATA FROM PICKLE FILES  ########
@@ -45,40 +46,40 @@ end = time.time()
 print(f"Finished loading in: {(end - start):.2f} seconds")
 del start, end
 
-##########################################
-###  VISUALIZING DATA  ###################
-##########################################
-
-from LWS.DataModels.LWSTrialVisualizer import LWSTrialVisualizer
-
-start = time.time()
-
-visualizer = LWSTrialVisualizer(screen_resolution=cnfg.SCREEN_MONITOR.resolution, output_directory=cnfg.OUTPUT_DIR)
-
-failed_trials = []
-for tr in subject.get_all_trials():
-    if tr.trial_num > 2:
-        break
-    try:
-        start_trial = time.time()
-        visualizer.create_gaze_figure(trial=tr, savefig=True)
-        visualizer.create_targets_figure(trial=tr, savefig=True)
-        visualizer.create_heatmap(trial=tr, savefig=True, fixation_only=True, show_targets_color=(0, 0, 0))
-        visualizer.create_heatmap(trial=tr, savefig=True, fixation_only=False, show_targets_color=(0, 0, 0))
-        visualizer.create_video(trial=tr, output_directory=cnfg.OUTPUT_DIR)
-        end_trial = time.time()
-        print(f"\t{tr.__repr__()}:\t{(end_trial - start_trial):.2f} s")
-    except Exception as e:
-        failed_trials.append((tr, e))
-        print(f"Failed to visualize trial: {tr.__repr__()}")
-
-end = time.time()
-print(f"Finished visualization in: {(end - start):.2f} seconds")
-
-# delete irrelevant variables:
-del start, end, start_trial, end_trial, tr
-if len(failed_trials) == 0:
-    del failed_trials
+# ##########################################
+# ###  VISUALIZING DATA  ###################
+# ##########################################
+#
+# from LWS.DataModels.LWSTrialVisualizer import LWSTrialVisualizer
+#
+# start = time.time()
+#
+# visualizer = LWSTrialVisualizer(screen_resolution=cnfg.SCREEN_MONITOR.resolution, output_directory=cnfg.OUTPUT_DIR)
+#
+# failed_trials = []
+# for tr in subject.get_all_trials():
+#     if tr.trial_num > 1:
+#         break
+#     try:
+#         start_trial = time.time()
+#         # visualizer.create_gaze_figure(trial=tr, savefig=True)
+#         # visualizer.create_targets_figure(trial=tr, savefig=True)
+#         visualizer.create_heatmap(trial=tr, savefig=True, fixation_only=True, show_targets_color=(0, 0, 0))
+#         visualizer.create_heatmap(trial=tr, savefig=True, fixation_only=True, show_targets_color=(0, 0, 0))
+#         # visualizer.create_video(trial=tr, output_directory=cnfg.OUTPUT_DIR)
+#         end_trial = time.time()
+#         print(f"\t{tr.__repr__()}:\t{(end_trial - start_trial):.2f} s")
+#     except Exception as e:
+#         failed_trials.append((tr, e))
+#         print(f"Failed to visualize trial: {tr.__repr__()}")
+#
+# end = time.time()
+# print(f"Finished visualization in: {(end - start):.2f} seconds")
+#
+# # delete irrelevant variables:
+# del start, end, start_trial, end_trial, tr
+# if len(failed_trials) == 0:
+#     del failed_trials
 
 ##########################################
 ###  ANALYZING DATA  #####################
@@ -86,25 +87,35 @@ if len(failed_trials) == 0:
 
 import LWS.analysis_scripts.trial_summary as trsum
 import LWS.analysis_scripts.events_summary as evsum
+import Visualization.saccade_analysis as sacan
+import Visualization.fixation_analysis as fixan
 
-start = time.time()
+# start = time.time()
 
 trials = subject.get_all_trials()
 
-trial_summary = trsum.summarize_all_trials(trials)
-
 all_blinks = [b for tr in trials for b in tr.get_gaze_events(cnst.BLINK)]
-blink_summary = evsum.summarize_events(all_blinks)
-
 all_saccades = [s for tr in trials for s in tr.get_gaze_events(cnst.SACCADE)]
-saccade_summary = evsum.summarize_events(all_saccades)
-
 all_fixations = [f for tr in trials for f in tr.get_gaze_events(cnst.FIXATION)]
-fixation_summary = evsum.summarize_events(all_fixations)
 
-end = time.time()
-print(f"Finished analysis in: {(end - start):.2f} seconds")
+sac_hists = sacan.saccade_histograms_figure(all_saccades, ignore_outliers=True)
+fix_hists = fixan.fixation_histograms_figure(all_fixations, ignore_outliers=True)
 
-del start, end
+visutils.show_figure(sac_hists)
+visutils.show_figure(fix_hists)
+
+start_points = [s.start_point for s in all_saccades if not np.isnan(s.start_point).any()]
+end_points = [s.end_point for s in all_saccades if not np.isnan(s.start_point).any()]
+
+
+# trial_summary = trsum.summarize_all_trials(trials)
+# blink_summary = evsum.summarize_events(all_blinks)
+# saccade_summary = evsum.summarize_events(all_saccades)
+# fixation_summary = evsum.summarize_events(all_fixations)
+
+# end = time.time()
+# print(f"Finished analysis in: {(end - start):.2f} seconds")
+#
+# del start, end
 
 
