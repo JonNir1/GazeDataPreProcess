5d4255cbdaeca14d8278e91da94207057697ed05
JonNir <jonathan.nir@mail.huji.ac.il>
2023-06-29 13:42:46 +0300

    change titles

--- Visualization/fixation_analysis.py
+++ Visualization/fixation_analysis.py
@@ -10,28 +10,28 @@ def fixation_histograms_figure(fixations: List[FixationEvent], ignore_outliers: 
         fixations = [f for f in fixations if not f.is_outlier]
 
     fig, axes = plt.subplots(2, 2, figsize=kwargs.get("figsize", (21, 14)))
-    fig.suptitle("Fixation Summary", y=0.98, font_size=kwargs.get("title_size", 16))
+    fig.suptitle(t="Fixation Summary", y=0.98, font_size=kwargs.get("title_size", 16))
 
     nbins = kwargs.get("nbins", 20)
     subtitle_size = kwargs.get("subtitle_size", 14)
     label_size = kwargs.get("label_size", 12)
 
     # durations histogram
-    ax = visutils.create_histogram([f.duration for f in fixations], axes[0, 0], title="Fixation Durations",
+    ax = visutils.create_histogram([f.duration for f in fixations], axes[0, 0], title="Durations",
                                    xlabel="Duration (ms)", color=kwargs.get("duration_color", "lightblue"),
                                    nbins=nbins, title_size=subtitle_size, label_size=label_size)
     # dispersion histogram
-    ax = visutils.create_histogram([f.max_dispersion for f in fixations], axes[0, 1], title="Fixation Dispersions",
+    ax = visutils.create_histogram([f.max_dispersion for f in fixations], axes[0, 1], title="Dispersions",
                                    xlabel="Dispersion (pixels)", color=kwargs.get("dispersion_color", "lightblue"),
                                    nbins=nbins, title_size=subtitle_size, label_size=label_size)
 
     # max velocity histogram
-    ax = visutils.create_histogram([f.max_velocity for f in fixations], axes[1, 0], title="Fixation Max Velocities",
+    ax = visutils.create_histogram([f.max_velocity for f in fixations], axes[1, 0], title="Max Velocities",
                                    xlabel="Max Velocity (pixels/ms)", color=kwargs.get("max_vel_color", "lightblue"),
                                    nbins=nbins, title_size=subtitle_size, label_size=label_size)
 
     # mean velocity histogram
-    ax = visutils.create_histogram([f.mean_velocity for f in fixations], axes[1, 1], title="Fixation Mean Velocities",
+    ax = visutils.create_histogram([f.mean_velocity for f in fixations], axes[1, 1], title="Mean Velocities",
                                    xlabel="Mean Velocity (pixels/ms)", color=kwargs.get("mean_vel_color", "lightblue"),
                                    nbins=nbins, title_size=subtitle_size, label_size=label_size)
     return fig
