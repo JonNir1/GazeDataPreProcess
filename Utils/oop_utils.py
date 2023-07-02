eca2be0b84d66fd923131aa3cb3f9f3b826b4207
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-02 13:59:07 +0300

    add function to oop_utils.py

--- Utils/oop_utils.py
+++ Utils/oop_utils.py
@@ -1,6 +1,16 @@
 import inspect
 
 
+def get_property(obj, attr: str):
+    """
+    Returns the value of `attr` if it is a public property of `obj`, otherwise raises an AttributeError.
+    """
+    if is_property(obj, attr):
+        return getattr(obj, attr)
+    else:
+        raise AttributeError(f"{type(obj).capitalize()} objects have no attribute `{attr}`")
+
+
 def is_property(obj, attr: str) -> bool:
     """
     Returns True if `attr` is a public property of `obj`.
