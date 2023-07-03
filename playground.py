e7d5dff4f1069ab8f1eb518d85d739744710de9b
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-03 14:28:21 +0300

    update

--- playground.py
+++ playground.py
@@ -41,15 +41,49 @@ import Visualization.visualization_utils as visutils
 start = time.time()
 
 subject = LWSSubject.from_pickle(os.path.join(cnfg.OUTPUT_DIR, "S002", "LWSSubject_002.pkl"))
+trials = subject.get_all_trials()
+all_blinks = [b for tr in trials for b in tr.get_gaze_events(cnst.BLINK)]
+all_saccades = [s for tr in trials for s in tr.get_gaze_events(cnst.SACCADE)]
+all_fixations = [f for tr in trials for f in tr.get_gaze_events(cnst.FIXATION)]
 
 end = time.time()
 print(f"Finished loading in: {(end - start):.2f} seconds")
 del start, end
 
+##########################################
+###  ANALYZE SUBJECT-LEVEL DATA  #########
+##########################################
+
+import LWS.analysis_scripts.trial_summary as trsum
+import Visualization.saccade_analysis as sacan
+import LWS.analysis_scripts.fixation_analysis as fixan
+
+start = time.time()
+
+trial_summary = trsum.summarize_all_trials(trials)
+close_target_fixations = [f for f in all_fixations if f.visual_angle_to_target < cnfg.THRESHOLD_VISUAL_ANGLE]
+mark_target_fixations = [f for f in all_fixations if f.is_mark_target_attempt]
+
+sac_hists = sacan.histograms_figure(all_saccades, ignore_outliers=True)
+fix_hists = fixan.histograms_figure(all_fixations, ignore_outliers=True,
+                                    title="Fixations Histograms")
+fix_dynamics = fixan.dynamics_figure(all_fixations, ignore_outliers=True,
+                                     title="Fixations Temporal Dynamics")
+
+visutils.show_figure(sac_hists)
+visutils.show_figure(fix_hists)
+visutils.show_figure(fix_dynamics)
+
+
+end = time.time()
+print(f"Finished analysis in: {(end - start):.2f} seconds")
+
+del start, end
+
 # ##########################################
-# ###  VISUALIZING DATA  ###################
+# ###  ANALYZE TRIAL-LEVEL DATA  ###########
 # ##########################################
-#
+
 # from LWS.DataModels.LWSTrialVisualizer import LWSTrialVisualizer
 #
 # start = time.time()
@@ -80,36 +114,5 @@ del start, end
 # del start, end, start_trial, end_trial, tr
 # if len(failed_trials) == 0:
 #     del failed_trials
-
-##########################################
-###  ANALYZING DATA  #####################
-##########################################
-
-import LWS.analysis_scripts.trial_summary as trsum
-import Visualization.saccade_analysis as sacan
-import LWS.analysis_scripts.fixation_analysis as fixan
-
-start = time.time()
-
-trials = subject.get_all_trials()
-trial_summary = trsum.summarize_all_trials(trials)
-
-# all_blinks = [b for tr in trials for b in tr.get_gaze_events(cnst.BLINK)]
-all_saccades = [s for tr in trials for s in tr.get_gaze_events(cnst.SACCADE)]
-all_fixations = [f for tr in trials for f in tr.get_gaze_events(cnst.FIXATION)]
-close_target_fixations = [f for f in all_fixations if f.visual_angle_to_target < cnfg.THRESHOLD_VISUAL_ANGLE]
-mark_target_fixations = [f for f in all_fixations if f.is_mark_target_attempt]
-
-sac_hists = sacan.saccade_histograms_figure(all_saccades, ignore_outliers=True)
-all_fix_hists = fixan.fixation_histograms_figure(all_fixations, ignore_outliers=True,
-                                                 title="All Fixations vs. Mark-Target Fixations")
-
-visutils.show_figure(sac_hists)
-visutils.show_figure(all_fix_hists)
-
-end = time.time()
-print(f"Finished analysis in: {(end - start):.2f} seconds")
-
-del start, end
 
 
