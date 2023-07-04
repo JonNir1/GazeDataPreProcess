8668ce2e496ef81243ba6c7f6d40eea1929b08ab
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-04 14:37:04 +0300

    comments + unused vars

--- Visualization/saccade_analysis.py
+++ Visualization/saccade_analysis.py
@@ -14,30 +14,27 @@ def distributions_figure(saccades: List[SaccadeEvent], ignore_outliers: bool = T
     fig = plt.Figure()
     visutils.set_figure_properties(fig, title=kwargs.pop("title", "Saccade Summary"),
                                    figsize=kwargs.pop("figsize", (21, 14)), **kwargs)
-    nbins = kwargs.get("nbins", 20)
-    edge_color = visutils.get_rgba_color(color=0, cmap_name=kwargs.get("cmap_name", "tab20"))
-    face_color = visutils.get_rgba_color(color=1, cmap_name=kwargs.get("cmap_name", "tab20"))
 
-    # durations histogram
+    # durations distribution
     ax1 = fig.add_subplot(2, 2, 1)
     durations_data = [np.array([s.duration for s in saccades])]
     distributions.bar_chart(ax=ax1, data=durations_data,
                             labels=["All Saccades"], title="Durations", xlabel="Duration (ms)", **kwargs)
 
-    # max velocity histogram
+    # max velocity distribution
     ax2 = fig.add_subplot(2, 2, 2)
     max_velocities_data = [np.array([s.max_velocity for s in saccades])]
     distributions.bar_chart(ax=ax2, data=max_velocities_data,
                             labels=["All Saccades"], title="Maximum Velocities", xlabel="Max Velocity (pixels/ms)",
                             **kwargs)
 
-    # amplitude histogram
+    # amplitude distribution
     ax3 = fig.add_subplot(2, 2, 3)
     amplitude_data = [np.array([s.amplitude for s in saccades])]
     distributions.bar_chart(ax=ax3, data=amplitude_data,
                             labels=["All Saccades"], title="Amplitude", xlabel="Amplitude (°)", **kwargs)
 
-    # azimuth counts (polar)
+    # azimuth distribution (polar)
     ax4 = fig.add_subplot(2, 2, 4, polar=True)
     azimuth_data = [np.array([s.azimuth for s in saccades])]
     distributions.rose_chart(ax=ax4, data=azimuth_data, labels=["All Saccades"], title="Azimuth (°)", **kwargs)
