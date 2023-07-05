4503a4b37b3e2c5cdaf58600b29eff47976d8cb4
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-05 15:57:07 +0300

    update

--- playground.py
+++ playground.py
@@ -46,10 +46,6 @@ all_blinks = [b for tr in trials for b in tr.get_gaze_events(cnst.BLINK)]
 all_saccades = [s for tr in trials for s in tr.get_gaze_events(cnst.SACCADE)]
 all_fixations = [f for tr in trials for f in tr.get_gaze_events(cnst.FIXATION)]
 
-target_proximal_fixations = [f for f in all_fixations if f.visual_angle_to_target <= cnfg.THRESHOLD_VISUAL_ANGLE]
-target_marking_fixations = [f for f in all_fixations if f.is_mark_target_attempt]
-target_proximal_non_marking_fixations = [f for f in target_proximal_fixations if not f.is_mark_target_attempt]
-
 end = time.time()
 print(f"Finished loading in: {(end - start):.2f} seconds")
 del start, end
@@ -58,46 +54,46 @@ del start, end
 ###  ANALYZE SUBJECT-LEVEL DATA  #########
 ##########################################
 
-import Utils.io_utils as ioutils
-import LWS.analysis_scripts.trial_summary as trsum
-import Visualization.saccade_analysis as sacan
-import LWS.analysis_scripts.fixation_analysis as fixan
-
-start = time.time()
-
-trial_summary = trsum.summarize_all_trials(trials)
-
-saccade_distributions = sacan.distributions_figure(all_saccades, ignore_outliers=True,
-                                                   title="Saccades Property Distributions", show_legend=True)
-fixation_distributions = fixan.distributions_figure(all_fixations, ignore_outliers=True,
-                                                    title="Fixations Property Distributions", show_legend=True)
-fixation_dynamics = fixan.dynamics_figure(all_fixations, ignore_outliers=True,
-                                          title="Fixations Temporal Dynamics", show_legend=True)
-fixation_proximity_comparison = fixan.target_proximal_comparison(all_fixations, ignore_outliers=True,
-                                                                 title="Fixations Proximity Comparison",
-                                                                 show_legend=True)
-
-subject_figures_dir = ioutils.create_directory(dirname="subject_figures", parent_dir=subject.output_dir)
-visutils.save_figure(saccade_distributions, full_path=os.path.join(subject_figures_dir, "saccade distributions.png"))
-visutils.save_figure(fixation_distributions, full_path=os.path.join(subject_figures_dir, "fixation distributions.png"))
-visutils.save_figure(fixation_dynamics, full_path=os.path.join(subject_figures_dir, "fixation dynamics.png"))
-visutils.save_figure(fixation_proximity_comparison,
-                     full_path=os.path.join(subject_figures_dir, "target-proximal fixation comparison.png"))
-
-visutils.show_figure(saccade_distributions)
+# import Utils.io_utils as ioutils
+# import LWS.analysis_scripts.trial_summary as trsum
+# import Visualization.saccade_analysis as sacan
+# import LWS.analysis_scripts.fixation_analysis as fixan
+#
+# start = time.time()
+#
+# trial_summary = trsum.summarize_all_trials(trials)
+#
+# saccade_distributions = sacan.distributions_figure(all_saccades, ignore_outliers=True,
+#                                                    title="Saccades Property Distributions", show_legend=True)
+# fixation_distributions = fixan.distributions_figure(all_fixations, ignore_outliers=True,
+#                                                     title="Fixations Property Distributions", show_legend=True)
+# fixation_dynamics = fixan.dynamics_figure(all_fixations, ignore_outliers=True,
+#                                           title="Fixations Temporal Dynamics", show_legend=True)
+# fixation_proximity_comparison = fixan.target_proximal_comparison(all_fixations, ignore_outliers=True,
+#                                                                  title="Fixations Proximity Comparison",
+#                                                                  show_legend=True)
+#
+# subject_figures_dir = ioutils.create_directory(dirname="subject_figures", parent_dir=subject.output_dir)
+# visutils.save_figure(saccade_distributions, full_path=os.path.join(subject_figures_dir, "saccade distributions.png"))
+# visutils.save_figure(fixation_distributions, full_path=os.path.join(subject_figures_dir, "fixation distributions.png"))
+# visutils.save_figure(fixation_dynamics, full_path=os.path.join(subject_figures_dir, "fixation dynamics.png"))
+# visutils.save_figure(fixation_proximity_comparison,
+#                      full_path=os.path.join(subject_figures_dir, "target-proximal fixation comparison.png"))
+#
+# visutils.show_figure(saccade_distributions)
 # visutils.show_figure(fixation_distributions)
 # visutils.show_figure(fixation_dynamics)
 # visutils.show_figure(fixation_proximity_comparison)
-
-end = time.time()
-print(f"Finished subject analysis in: {(end - start):.2f} seconds")
-
-del start, end, subject_figures_dir
+#
+# end = time.time()
+# print(f"Finished subject analysis in: {(end - start):.2f} seconds")
+#
+# del start, end, subject_figures_dir
 
 # ##########################################
 # ###  ANALYZE TRIAL-LEVEL DATA  ###########
 # ##########################################
-
+#
 # from LWS.DataModels.LWSTrialVisualizer import LWSTrialVisualizer
 #
 # start = time.time()
@@ -106,15 +102,15 @@ del start, end, subject_figures_dir
 #
 # failed_trials = []
 # for tr in subject.get_all_trials():
-#     if tr.trial_num > 1:
+#     if tr.trial_num > 1:  # TODO: remove
 #         break
 #     try:
 #         start_trial = time.time()
-#         # visualizer.create_gaze_figure(trial=tr, savefig=True)
-#         # visualizer.create_targets_figure(trial=tr, savefig=True)
+#         visualizer.create_gaze_figure(trial=tr, savefig=True)
+#         visualizer.create_targets_figure(trial=tr, savefig=True)
+#         visualizer.create_heatmap(trial=tr, savefig=True, fixation_only=False, show_targets_color=(0, 0, 0))
 #         visualizer.create_heatmap(trial=tr, savefig=True, fixation_only=True, show_targets_color=(0, 0, 0))
-#         visualizer.create_heatmap(trial=tr, savefig=True, fixation_only=True, show_targets_color=(0, 0, 0))
-#         # visualizer.create_video(trial=tr, output_directory=cnfg.OUTPUT_DIR)
+#         visualizer.create_video(trial=tr, output_directory=cnfg.OUTPUT_DIR)
 #         end_trial = time.time()
 #         print(f"\t{tr.__repr__()}:\t{(end_trial - start_trial):.2f} s")
 #     except Exception as e:
