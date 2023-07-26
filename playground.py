9b3116fdaee02c1402cd859b91dc050527bdb589
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-26 19:50:10 +0300

    move playground code to a helper file to clean up the pg

--- playground.py
+++ playground.py
@@ -8,128 +8,29 @@ import cv2
 # from typing import Optional, Tuple, List, Union, Dict
 
 import constants as cnst
+import playground_helpers as ph
 from Config import experiment_config as cnfg
-from LWS.DataModels.LWSSubject import LWSSubject
 import Visualization.visualization_utils as visutils
 
 ##########################################
-### PREPROCESSING GAZE DATA  #############
+### RUN THE PIPELINE  ####################
 ##########################################
 
-import LWS.PreProcessing as pp
+# subjects: "GalChen Demo" (001), "Rotem Demo" (002)
+# subject = ph.process_subject(name="GalChen Demo", save=True, verbose=True)
 
-start = time.time()
+subject = ph.load_subject(subject_id=1, verbose=True)
 
-subject = pp.process_subject(subject_dir=os.path.join(cnfg.RAW_DATA_DIR, 'GalChen Demo'),
-                             screen_monitor=cnfg.SCREEN_MONITOR,
-                             save_pickle=False,
-                             stuff_with='fixation',
-                             blink_detector_type='missing data',
-                             saccade_detector_type='engbert',
-                             drop_outlier_events=False)
+subject_analysis = ph.analyze_subject(subject, save=True, verbose=True)
+trial_summary, saccade_distributions, fixation_distributions, fixation_dynamics, fixation_proximity_comparison = subject_analysis
+del subject_analysis
 
-end = time.time()
-print(f"Finished preprocessing in: {(end - start):.2f} seconds")
-
-# delete irrelevant variables:
-del start, end
+ph.visualize_all_trials(subject, save=True, verbose=True)
 
 ##########################################
-###  LOAD DATA FROM PICKLE FILES  ########
-##########################################
-
-start = time.time()
-
-subject = LWSSubject.from_pickle(os.path.join(cnfg.OUTPUT_DIR, "S001", "LWSSubject_001.pkl"))
-trials = subject.get_all_trials()
-all_blinks = [b for tr in trials for b in tr.get_gaze_events(cnst.BLINK)]
-all_saccades = [s for tr in trials for s in tr.get_gaze_events(cnst.SACCADE)]
-all_fixations = [f for tr in trials for f in tr.get_gaze_events(cnst.FIXATION)]
-
-end = time.time()
-print(f"Finished loading in: {(end - start):.2f} seconds")
-del start, end
-
-##########################################
-###  Playground  #########################
+### PLAYGROUND  ##########################
 ##########################################
 
 
-
-
-# ##########################################
-# ###  ANALYZE SUBJECT-LEVEL DATA  #########
-# ##########################################
-#
-# import Utils.io_utils as ioutils
-# import LWS.analysis_scripts.trial_summary as trsum
-# import Visualization.saccade_analysis as sacan
-# import LWS.analysis_scripts.fixation_analysis as fixan
-#
-# start = time.time()
-#
-# trial_summary = trsum.summarize_all_trials(trials)
-# trial_summary.to_pickle(os.path.join(subject.output_dir, "trials_summary.pkl"))
-#
-# saccade_distributions = sacan.distributions_figure(all_saccades, ignore_outliers=True,
-#                                                    title="Saccades Property Distributions", show_legend=True)
-# fixation_distributions = fixan.distributions_figure(all_fixations, ignore_outliers=True,
-#                                                     title="Fixations Property Distributions", show_legend=True)
-# fixation_dynamics = fixan.dynamics_figure(all_fixations, ignore_outliers=True,
-#                                           title="Fixations Temporal Dynamics", show_legend=True)
-# fixation_proximity_comparison = fixan.target_proximal_comparison(all_fixations, ignore_outliers=True,
-#                                                                  title="Fixations Proximity Comparison",
-#                                                                  show_legend=True)
-#
-# subject_figures_dir = ioutils.create_directory(dirname="subject_figures", parent_dir=subject.output_dir)
-# visutils.save_figure(saccade_distributions, full_path=os.path.join(subject_figures_dir, "saccade distributions.png"))
-# visutils.save_figure(fixation_distributions, full_path=os.path.join(subject_figures_dir, "fixation distributions.png"))
-# visutils.save_figure(fixation_dynamics, full_path=os.path.join(subject_figures_dir, "fixation dynamics.png"))
-# visutils.save_figure(fixation_proximity_comparison,
-#                      full_path=os.path.join(subject_figures_dir, "target-proximal fixation comparison.png"))
-#
-# visutils.show_figure(saccade_distributions)
-# visutils.show_figure(fixation_distributions)
-# visutils.show_figure(fixation_dynamics)
-# visutils.show_figure(fixation_proximity_comparison)
-#
-# end = time.time()
-# print(f"Finished subject analysis in: {(end - start):.2f} seconds")
-#
-# del start, end, subject_figures_dir
-
-# ##########################################
-# ###  ANALYZE TRIAL-LEVEL DATA  ###########
-# ##########################################
-#
-# from LWS.DataModels.LWSTrialVisualizer import LWSTrialVisualizer
-#
-# start = time.time()
-#
-# visualizer = LWSTrialVisualizer(screen_resolution=cnfg.SCREEN_MONITOR.resolution, output_directory=cnfg.OUTPUT_DIR)
-#
-# failed_trials = []
-# for tr in subject.get_all_trials():
-#     try:
-#         start_trial = time.time()
-#         visualizer.create_gaze_figure(trial=tr, savefig=True)
-#         visualizer.create_targets_figure(trial=tr, savefig=True)
-#         visualizer.create_heatmap(trial=tr, savefig=True, fixation_only=False, show_targets_color=(0, 0, 0))
-#         visualizer.create_heatmap(trial=tr, savefig=True, fixation_only=True, show_targets_color=(0, 0, 0))
-#         visualizer.create_video(trial=tr, output_directory=cnfg.OUTPUT_DIR)
-#         plt.close('all')  # close all open figures from memory
-#         end_trial = time.time()
-#         print(f"\t{tr.__repr__()}:\t{(end_trial - start_trial):.2f} s")
-#     except Exception as e:
-#         failed_trials.append((tr, e))
-#         print(f"Failed to visualize trial: {tr.__repr__()}")
-#
-# end = time.time()
-# print(f"Finished subject's trials analysis in: {(end - start):.2f} seconds")
-#
-# # delete irrelevant variables:
-# del start, end, start_trial, end_trial, tr
-# if len(failed_trials) == 0:
-#     del failed_trials
 
 
---
+++ playground_helpers.py
@@ -0,0 +1,108 @@
+import os
+import time
+import matplotlib.pyplot as plt
+
+import constants as cnst
+import Config.experiment_config as cnfg
+from LWS.DataModels.LWSSubject import LWSSubject
+import Visualization.visualization_utils as visutils
+
+
+def process_subject(name: str, save: bool = False, verbose: bool = True) -> LWSSubject:
+    start = time.time()
+    import LWS.PreProcessing as pp
+    subject = pp.process_subject(subject_dir=os.path.join(cnfg.RAW_DATA_DIR, name),
+                                 screen_monitor=cnfg.SCREEN_MONITOR,
+                                 save_pickle=save,
+                                 stuff_with='fixation',
+                                 blink_detector_type='missing data',
+                                 saccade_detector_type='engbert',
+                                 drop_outlier_events=False)
+    end = time.time()
+    if verbose:
+        print(f"Finished preprocessing subject {name}: {(end - start):.2f} seconds")
+    return subject
+
+
+def load_subject(subject_id: int, verbose: bool = True) -> LWSSubject:
+    start = time.time()
+    subject_id = f"{subject_id:03d}"
+    subdir = f"S{subject_id}"
+    subject = LWSSubject.from_pickle(os.path.join(cnfg.OUTPUT_DIR, subdir, f"LWSSubject_{subject_id}.pkl"))
+    end = time.time()
+    if verbose:
+        print(f"Finished loading subject {subject_id}: {(end - start):.2f} seconds")
+    return subject
+
+
+def analyze_subject(subject: LWSSubject, save: bool = False, verbose: bool = True):
+    start = time.time()
+    import Utils.io_utils as ioutils
+    import LWS.analysis_scripts.trial_summary as trsum
+    import Visualization.saccade_analysis as sacan
+    import LWS.analysis_scripts.fixation_analysis as fixan
+
+    trials = subject.get_all_trials()
+    all_saccades = [s for tr in trials for s in tr.get_gaze_events(cnst.SACCADE)]
+    all_fixations = [f for tr in trials for f in tr.get_gaze_events(cnst.FIXATION)]
+
+    trial_summary = trsum.summarize_all_trials(trials)
+    saccade_distributions = sacan.distributions_figure(all_saccades, ignore_outliers=True,
+                                                       title="Saccades Property Distributions", show_legend=True)
+    fixation_distributions = fixan.distributions_figure(all_fixations, ignore_outliers=True,
+                                                        title="Fixations Property Distributions", show_legend=True)
+    fixation_dynamics = fixan.dynamics_figure(all_fixations, ignore_outliers=True,
+                                              title="Fixations Temporal Dynamics", show_legend=True)
+    fixation_proximity_comparison = fixan.target_proximal_comparison(all_fixations, ignore_outliers=True,
+                                                                     title="Fixations Proximity Comparison",
+                                                                     show_legend=True)
+    if save:
+        trial_summary.to_pickle(os.path.join(subject.output_dir, "trials_summary.pkl"))
+        subject_figures_dir = ioutils.create_directory(dirname="subject_figures", parent_dir=subject.output_dir)
+        visutils.save_figure(saccade_distributions,
+                             full_path=os.path.join(subject_figures_dir, "saccade distributions.png"))
+        visutils.save_figure(fixation_distributions,
+                             full_path=os.path.join(subject_figures_dir, "fixation distributions.png"))
+        visutils.save_figure(fixation_dynamics, full_path=os.path.join(subject_figures_dir, "fixation dynamics.png"))
+        visutils.save_figure(fixation_proximity_comparison,
+                             full_path=os.path.join(subject_figures_dir, "target-proximal fixation comparison.png"))
+    end = time.time()
+    if verbose:
+        print(f"Finished analyzing subject {subject.subject_id}: {(end - start):.2f} seconds")
+    return trial_summary, saccade_distributions, fixation_distributions, fixation_dynamics, fixation_proximity_comparison
+
+
+def visualize_all_trials(subject: LWSSubject, save: bool = False, verbose: bool = True):
+    start = time.time()
+    from LWS.TrialVisualizer.LWSTrialGazeVisualizer import LWSTrialGazeVisualizer
+    from LWS.TrialVisualizer.LWSTrialTargetDistancesVisualizer import LWSTrialTargetDistancesVisualizer
+    from LWS.TrialVisualizer.LWSTrialHeatmapVisualizer import LWSTrialGazeHeatmapVisualizer, LWSTrialFixationsHeatmapVisualizer
+    from LWS.TrialVisualizer.LWSTrialVideoVisualizer import LWSTrialVideoVisualizer
+
+    failed_trials = []
+    for tr in subject.get_all_trials():
+        try:
+            start_trial = time.time()
+            gaze = LWSTrialGazeVisualizer(screen_resolution=cnfg.SCREEN_MONITOR.resolution,
+                                          output_directory=cnfg.OUTPUT_DIR).visualize(tr, should_save=save)
+            targets = LWSTrialTargetDistancesVisualizer(screen_resolution=cnfg.SCREEN_MONITOR.resolution,
+                                                        output_directory=cnfg.OUTPUT_DIR).visualize(tr, should_save=save)
+            gaze_heatmap = LWSTrialGazeHeatmapVisualizer(screen_resolution=cnfg.SCREEN_MONITOR.resolution,
+                                                         output_directory=cnfg.OUTPUT_DIR).visualize(tr, should_save=save)
+            fixation_heatmap = LWSTrialFixationsHeatmapVisualizer(screen_resolution=cnfg.SCREEN_MONITOR.resolution,
+                                                                  output_directory=cnfg.OUTPUT_DIR).visualize(tr, should_save=save)
+            video = LWSTrialVideoVisualizer(screen_resolution=cnfg.SCREEN_MONITOR.resolution,
+                                            output_directory=cnfg.OUTPUT_DIR).visualize(tr, should_save=save)
+            plt.close('all')  # close all open figures from memory
+            end_trial = time.time()
+            if verbose:
+                print(f"\t{tr.__repr__()}:\t{(end_trial - start_trial):.2f} s")
+        except Exception as e:
+            failed_trials.append((tr, e))
+            if verbose:
+                print(f"Failed to visualize trial {tr.__repr__()}:\n{e}\n")
+
+    end = time.time()
+    if verbose:
+        print(f"Finished visualizing all trials: {(end - start):.2f} seconds")
+    return failed_trials
