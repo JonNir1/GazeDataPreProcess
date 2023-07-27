efec21951009f9fa06fe099d822edca0fd201dc2
JonNir <jonathan.nir@mail.huji.ac.il>
2023-07-27 16:39:59 +0300

    update

--- playground.py
+++ playground.py
@@ -13,13 +13,16 @@ from Config import experiment_config as cnfg
 import Visualization.visualization_utils as visutils
 
 ##########################################
-### RUN THE PIPELINE  ####################
+### RUN PIPELINE / LOAD DATA  ############
 ##########################################
 
 # subjects: "GalChen Demo" (001), "Rotem Demo" (002)
 
-subject1, subject1_analysis, failed_trials1 = ph.full_pipline(name="GalChen Demo", save=True, verbose=True)
-subject2, subject2_analysis, failed_trials2 = ph.full_pipline(name="Rotem Demo", save=True, verbose=True)
+# subject1, subject1_analysis, failed_trials1 = ph.full_pipline(name="GalChen Demo", save=True, verbose=True)
+# subject2, subject2_analysis, failed_trials2 = ph.full_pipline(name="Rotem Demo", save=True, verbose=True)
+
+subject1 = ph.load_subject(subject_id=1, verbose=True)
+subject2 = ph.load_subject(subject_id=2, verbose=True)
 
 
 ##########################################
