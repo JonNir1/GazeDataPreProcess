e739b41e3b85ad1c83cc9c96a334154cbf66c841
JonNir <jonathan.nir@mail.huji.ac.il>
2023-08-01 15:07:05 +0300

    update input types

--- Utils/calculate_sampling_rate.py
+++ Utils/calculate_sampling_rate.py
@@ -5,15 +5,13 @@ from typing import Union
 import constants as cnst
 
 
-def calculate_sampling_rate_from_milliseconds(milliseconds: Union[np.ndarray, pd.Series]) -> float:
-    if isinstance(milliseconds, np.ndarray):
-        milliseconds = pd.Series(milliseconds)
+def calculate_sampling_rate_from_milliseconds(milliseconds: np.ndarray) -> float:
+    milliseconds = pd.Series(milliseconds)
     return cnst.MILLISECONDS_PER_SECOND / milliseconds.diff().mean()
 
 
-def calculate_sampling_rate_from_microseconds(microseconds: Union[np.ndarray, pd.Series]) -> float:
-    if isinstance(microseconds, np.ndarray):
-        microseconds = pd.Series(microseconds)
+def calculate_sampling_rate_from_microseconds(microseconds: np.ndarray) -> float:
+    microseconds = pd.Series(microseconds)
     return cnst.MICROSECONDS_PER_SECOND / microseconds.diff().mean()
 
 
