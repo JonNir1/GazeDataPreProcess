5aa2ba768f3685aebd751c32569ce9c306a75b22
JonNir <jonathan.nir@mail.huji.ac.il>
2023-08-15 17:55:20 +0300

    update

--- playground.py
+++ playground.py
@@ -17,12 +17,12 @@ import Visualization.visualization_utils as visutils
 ##########################################
 
 # subjects: "GalChen Demo" (001), "Rotem Demo" (002)
-pipline_config = {'save': True, 'skip_analysis': True, 'verbose': True}
+pipline_config = {'save': True, 'skip_analysis': False, 'verbose': True}
 
 subject1, subject1_analysis, failed_trials1 = ph.full_pipline(name="GalChen Demo", **pipline_config)
-del subject1_analysis, failed_trials1
+# del subject1_analysis, failed_trials1
 
-# subject2, subject2_analysis, failed_trials2 = ph.full_pipline(name="Rotem Demo", **pipline_config)
+subject2, subject2_analysis, failed_trials2 = ph.full_pipline(name="Rotem Demo", **pipline_config)
 # del subject2_analysis, failed_trials2
 
 del pipline_config
@@ -35,14 +35,10 @@ del pipline_config
 ### PLAYGROUND  ##########################
 ##########################################
 
-import LWS.analysis_scripts.looking_without_seeing as lws
+from LWS.subject_analysis.lws_figures import lws_rates_figure
 
-all_trials = subject1.get_all_trials()
 thresholds = np.arange(0.1 * cnfg.THRESHOLD_VISUAL_ANGLE,
                        1.2 * cnfg.THRESHOLD_VISUAL_ANGLE,
                        0.1 * cnfg.THRESHOLD_VISUAL_ANGLE)
-lws_counts = np.zeros((subject1.num_trials, len(thresholds)))
-
-for i, thrsh in enumerate(thresholds):
-    lws_counts[:, i] = [lws.count_lws_fixations_per_trial(trial, proximity_threshold=thrsh) for trial in all_trials]
-
+fig = lws_rates_figure(subject1, thresholds)
+visutils.show_figure(fig)
