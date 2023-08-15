86ddd6737baff136d2fedd878dde34032b831849
JonNir <jonathan.nir@mail.huji.ac.il>
2023-08-15 10:34:57 +0300

    get rid of the target identification data in LWSTrial.py

--- LWS/DataModels/LWSTrial.py
+++ LWS/DataModels/LWSTrial.py
@@ -96,37 +96,15 @@ class LWSTrial:
     def get_stimulus_image(self, color_format: str = 'bgr') -> np.ndarray:
         return self.__stimulus.get_image(color_format=color_format)
 
-    def get_targets(self, proximity_threshold: Optional[float] = None) -> pd.DataFrame:
+    def get_targets(self) -> pd.DataFrame:
         """
-        Returns a dataframe containing information about the trial's targets.
-        If `proximity_threshold` is set to None, only the basic information about the targets is returned. Otherwise,
-        additional information regarding the targets' identification by the subject is also returned.
-
-        :param proximity_threshold: None or positive float; if not None, this sets the maximum distance (in visual angle)
-            between the target and the gaze for the target to be considered identified by the subject (when they attempt
-            to mark a target using ExperimentTriggerEnum.MARK_TARGET_SUCCESSFUL).
-
-        :return: a dataframe with shape (num_targets, 4) if proximity_threshold is None, or (num_targets, 7) otherwise.
-        :raise: ValueError if proximity_threshold is non-positive.
-
-        Basic information:
+        Returns a dataframe containing information about the trial's targets:
             - icon_path: full path to the icon file
             - icon_category: category of the icon (face, animal, etc.)
             - center_x: x coordinate of the icon center
             - center_y: y coordinate of the icon center
-
-        Additional information:
-            - distance_identified: distance (in visual angle) between the target and the gaze when the target was
-                identified by the subject
-            - time_identified: time (in milliseconds) when the target was identified by the subject
-            - time_confirmed: time (in milliseconds) when the target was confirmed by the subject
         """
-        targets_df = self.__stimulus.get_target_data()
-        if proximity_threshold is None:
-            return targets_df
-        target_identification_data = self._extract_target_identification_data(proximity_threshold=proximity_threshold)
-        final_df = pd.concat([targets_df, target_identification_data], axis=1)
-        return final_df
+        return self.__stimulus.get_target_data()
 
     def get_behavioral_data(self) -> LWSBehavioralData:
         return self.__behavioral_data
@@ -217,62 +195,6 @@ class LWSTrial:
         with open(full_path, "wb") as f:
             pkl.dump(self, f)
         return full_path
-
-    def _extract_target_identification_data(self,
-                                            proximity_threshold: float = cnfg.THRESHOLD_VISUAL_ANGLE,
-                                            identification_seq: np.ndarray = cnfg.TARGET_IDENTIFICATION_SEQUENCE) -> pd.DataFrame:
-        """
-        For each of the trial's targets, extracts the following information:
-            - distance_identified: distance (in visual angle) between the target and the gaze when the target was
-                identified by the subject
-            - time_identified: time (in milliseconds) when the target was identified by the subject
-            - time_confirmed: time (in milliseconds) when the target was confirmed by the subject
-
-        Returns a dataframe with shape (num_targets, 3), where each row corresponds to a target.
-        """
-        if proximity_threshold is not None and (proximity_threshold <= 0 or np.isinf(proximity_threshold)):
-            raise ValueError(f"Invalid `proximity_threshold`: {proximity_threshold}")
-        if np.isnan(proximity_threshold):
-            proximity_threshold = None
-
-        # extract relevant columns from the behavioral data
-        behavioral_data = self.get_behavioral_data()
-        columns = ([cnst.MICROSECONDS, cnst.TRIGGER, "closest_target"] +
-                   [col for col in behavioral_data.columns if col.startswith(f"{cnst.DISTANCE}_{cnst.TARGET}")])
-        behavioral_df = pd.DataFrame(behavioral_data.get(columns), columns=columns)
-
-        res = pd.DataFrame(np.full((self.num_targets, 3), np.nan),
-                           columns=["distance_identified", "time_identified", "time_confirmed"])
-        for i in range(self.num_targets):
-            proximal_behavioral_df = behavioral_df[behavioral_df["closest_target"] == i + 1]
-
-            # check if target was ever identified by the subject
-            identification_idxs = au.find_sequences_in_sparse_array(proximal_behavioral_df[cnst.TRIGGER].values,
-                                                                    sequence=identification_seq)
-            if len(identification_idxs) == 0:
-                # this target was never identified
-                continue
-
-            # check if any of the target's identification attempts were from below the threshold distance
-            identification_distances = np.array(
-                [proximal_behavioral_df.iloc[first_idx][f"{cnst.DISTANCE}_{cnst.TARGET}{i + 1}"]
-                 for first_idx, last_idx in identification_idxs])
-            proximal_identifications = np.where(identification_distances < proximity_threshold)[0]
-            if len(proximal_identifications) == 0:
-                # no proximal identification attempts
-                continue
-
-            # find the start & end idxs of the first identification attempt that was from below the threshold distance
-            first_proximal_identification = min(proximal_identifications)
-            first_proximal_identification_idxs = identification_idxs[first_proximal_identification]
-            first_idx, last_idx = first_proximal_identification_idxs
-            res.loc[i, "distance_identified"] = proximal_behavioral_df.iloc[first_idx][
-                f"{cnst.DISTANCE}_{cnst.TARGET}{i + 1}"]
-            res.loc[i, "time_identified"] = proximal_behavioral_df.iloc[first_idx][
-                                                       cnst.MICROSECONDS] / cnst.MICROSECONDS_PER_MILLISECOND
-            res.loc[i, "time_confirmed"] = proximal_behavioral_df.iloc[last_idx][
-                                                      cnst.MICROSECONDS] / cnst.MICROSECONDS_PER_MILLISECOND
-        return res
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}_{str(self.subject)}_{str(self)}"
