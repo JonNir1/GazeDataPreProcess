bc8f3fb72b2965e3e68057c1ade02b7013134076
JonNir <jonathan.nir@mail.huji.ac.il>
2023-08-15 10:58:50 +0300

    change blink max duration

--- GazeEvents/BlinkEvent.py
+++ GazeEvents/BlinkEvent.py
@@ -4,6 +4,6 @@ from GazeEvents.GazeEventEnums import GazeEventTypeEnum
 
 class BlinkEvent(BaseGazeEvent):
     _EVENT_TYPE = GazeEventTypeEnum.BLINK
-    MIN_DURATION = 50  # minimum duration of a blink in milliseconds
-    MAX_DURATION = 1000  # maximum duration of a blink in milliseconds
+    MIN_DURATION = 50    # (milliseconds)
+    MAX_DURATION = 500  # (milliseconds)
 
