b688b73d7ace3f8ef860d9bea39273ead8f51744
JonNir <jonathan.nir@mail.huji.ac.il>
2023-08-28 17:38:09 +0300

    nit

--- LWS/subject_analysis/lws_instances.py
+++ LWS/subject_analysis/lws_instances.py
@@ -46,30 +46,6 @@ def identify_lws_for_varying_thresholds(subject: LWSSubject,
     return is_lws_instance
 
 
-def calculate_lws_rate(trial: LWSTrial,
-                       proximity_threshold: float = cnfg.THRESHOLD_VISUAL_ANGLE,
-                       time_difference_threshold: float = SaccadeEvent.MAX_DURATION,
-                       proximal_fixations_only: bool = False) -> float:
-    """
-    Calculates the LWS rate for the given trial, which is the fraction of fixations that are LWS instances out of
-    (a) all fixations in the trial; or (b) only the proximal fixations in the trial, depending on the value of the flag
-    `proximal_fixations_only`.
-    """
-    is_lws_instance = load_or_identify_lws_instances(trial,
-                                                     proximity_threshold=proximity_threshold,
-                                                     time_difference_threshold=time_difference_threshold)
-    num_lws_instances = np.nansum(is_lws_instance)
-    fixations = trial.get_gaze_events(event_type=GazeEventTypeEnum.FIXATION)
-    if proximal_fixations_only:
-        fixations = list(filter(lambda f: f.visual_angle_to_closest_target <= proximity_threshold, fixations))
-    num_fixations = len(fixations)
-    if num_fixations > 0:
-        return num_lws_instances / num_fixations
-    if num_lws_instances == 0 and num_fixations == 0:
-        return np.nan
-    raise ZeroDivisionError(f"num_lws_instances = {num_lws_instances},\tnum_fixations = {num_fixations}")
-
-
 def load_or_identify_lws_instances(trial: LWSTrial,
                                    proximity_threshold,
                                    time_difference_threshold) -> List[Union[bool, float]]:
@@ -93,6 +69,30 @@ def load_or_identify_lws_instances(trial: LWSTrial,
     df.loc[trial, (proximity_threshold, time_difference_threshold)] = is_lws_instance
     df.to_pickle(df_path)
     return is_lws_instance
+
+
+def calculate_lws_rate(trial: LWSTrial,
+                       proximity_threshold: float = cnfg.THRESHOLD_VISUAL_ANGLE,
+                       time_difference_threshold: float = SaccadeEvent.MAX_DURATION,
+                       proximal_fixations_only: bool = False) -> float:
+    """
+    Calculates the LWS rate for the given trial, which is the fraction of fixations that are LWS instances out of
+    (a) all fixations in the trial; or (b) only the proximal fixations in the trial, depending on the value of the flag
+    `proximal_fixations_only`.
+    """
+    is_lws_instance = load_or_identify_lws_instances(trial,
+                                                     proximity_threshold=proximity_threshold,
+                                                     time_difference_threshold=time_difference_threshold)
+    num_lws_instances = np.nansum(is_lws_instance)
+    fixations = trial.get_gaze_events(event_type=GazeEventTypeEnum.FIXATION)
+    if proximal_fixations_only:
+        fixations = list(filter(lambda f: f.visual_angle_to_closest_target <= proximity_threshold, fixations))
+    num_fixations = len(fixations)
+    if num_fixations > 0:
+        return num_lws_instances / num_fixations
+    if num_lws_instances == 0 and num_fixations == 0:
+        return np.nan
+    raise ZeroDivisionError(f"num_lws_instances = {num_lws_instances},\tnum_fixations = {num_fixations}")
 
 
 def _identify_lws_instances(trial: LWSTrial,
