1f6d3c9013ab649b15d76fefefc675d95a6f380f
JonNir <jonathan.nir@mail.huji.ac.il>
2023-08-28 18:13:57 +0300

    update

--- playground.py
+++ playground.py
@@ -17,16 +17,16 @@ import Visualization.visualization_utils as visutils
 ##########################################
 
 # subjects: "GalChen Demo" (001), "Rotem Demo" (002)
-pipline_config = {'save': True, 'include_subject_dfs': True, 'include_subject_figures': True,
-                  'include_trial_figures': True, 'include_trial_videos': True, 'verbose': True}
-
-subject1, subject1_analysis, failed_trials1 = ph.full_pipline(name="GalChen Demo", **pipline_config)
-del subject1_analysis, failed_trials1
-
-subject2, subject2_analysis, failed_trials2 = ph.full_pipline(name="Rotem Demo", **pipline_config)
-del subject2_analysis, failed_trials2
-
-del pipline_config
+# pipline_config = {'save': True, 'include_subject_dfs': True, 'include_subject_figures': True,
+#                   'include_trial_figures': True, 'include_trial_videos': True, 'verbose': True}
+#
+# subject1, subject1_analysis, failed_trials1 = ph.full_pipline(name="GalChen Demo", **pipline_config)
+# del subject1_analysis, failed_trials1
+#
+# subject2, subject2_analysis, failed_trials2 = ph.full_pipline(name="Rotem Demo", **pipline_config)
+# del subject2_analysis, failed_trials2
+#
+# del pipline_config
 
 subject1 = ph.load_subject(subject_id=1, verbose=True)
 subject2 = ph.load_subject(subject_id=2, verbose=True)
