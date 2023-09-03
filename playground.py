facef982eb17df9d7225e24bbc5db20639f2c467
JonNir <jonathan.nir@mail.huji.ac.il>
2023-09-03 10:59:50 +0300

    update

--- playground.py
+++ playground.py
@@ -16,20 +16,21 @@ import Visualization.visualization_utils as visutils
 ### RUN PIPELINE / LOAD DATA  ############
 ##########################################
 
-# subjects: "GalChen Demo" (001), "Rotem Demo" (002)
+# subjects: "GalChen Demo" (001), "Rotem Demo" (002), "Netta Demo" (003)
 # pipline_config = {'save': True, 'include_subject_dfs': True, 'include_subject_figures': True,
 #                   'include_trial_figures': True, 'include_trial_videos': True, 'verbose': True}
 #
 # subject1, subject1_dfs, subject1_figures, failed_trials1 = ph.full_pipline(name="GalChen Demo", **pipline_config)
-# del subject1_analysis, failed_trials1
 #
 # subject2, subject2_dfs, subject2_figures, failed_trials2 = ph.full_pipline(name="Rotem Demo", **pipline_config)
-# del subject2_analysis, failed_trials2
 #
-# del pipline_config
+# subject3, subject3_dfs, subject3_figures, failed_trials3 = ph.full_pipline(name="Netta Demo", **pipline_config)
+#
+# del pipline_config, subject1_figures, failed_trials1, subject2_figures, failed_trials2
 
 subject1 = ph.load_subject(subject_id=1, verbose=True)
 subject2 = ph.load_subject(subject_id=2, verbose=True)
+subject3 = ph.load_subject(subject_id=3, verbose=True)
 
 
 ##########################################
