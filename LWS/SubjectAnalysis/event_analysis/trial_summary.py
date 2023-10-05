2f190fbab587018666772f048d1d911c99d8a473
JonNir <jonathan.nir@mail.huji.ac.il>
2023-10-05 10:58:08 +0300

    set DF name

--- LWS/SubjectAnalysis/event_analysis/trial_summary.py
+++ LWS/SubjectAnalysis/event_analysis/trial_summary.py
@@ -36,6 +36,7 @@ def summarize_all_trials(trials: List[LWSTrial], catch_exceptions=False, catch_w
                 raise e
     df = pd.DataFrame(series_list).set_index(cnst.TRIAL)
     df.index = df.index.astype(int)  # convert trial number to int
+    df.name = DF_NAME
     return df
 
 
