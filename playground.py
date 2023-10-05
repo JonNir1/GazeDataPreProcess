87caf0510190889b7041f09e73f25e47b8dcb60c
JonNir <jonathan.nir@mail.huji.ac.il>
2023-10-05 16:30:17 +0300

    update

--- playground.py
+++ playground.py
@@ -18,20 +18,20 @@ import Visualization.visualization_utils as visutils
 ##########################################
 
 # subjects: "GalChen Demo" (001), "Rotem Demo" (002), "Netta Demo" (003)
-pipline_config = {'save': True, 'include_subject_dfs': True, 'include_subject_figures': True,
+pipline_config = {'save': True, 'include_subject_figures': True,
                   'include_trial_figures': False, 'include_trial_videos': False, 'verbose': True}
 
-subject1, subject1_dfs, subject1_figures, failed_trials1 = ph.full_pipline(name="GalChen Demo", **pipline_config)
+subject1, subject1_figures, failed_trials1 = ph.full_pipline(name_or_id="GalChen Demo", **pipline_config)
 
-subject2, subject2_dfs, subject2_figures, failed_trials2 = ph.full_pipline(name="Rotem Demo", **pipline_config)
+# subject2, subject2_figures, failed_trials2 = ph.full_pipline(name_or_id="Rotem Demo", **pipline_config)
+#
+# subject3, subject3_figures, failed_trials3 = ph.full_pipline(name_or_id="Netta Demo", **pipline_config)
+#
+# del pipline_config, subject1_figures, failed_trials1, subject2_figures, failed_trials2, subject3_figures, failed_trials3
 
-subject3, subject3_dfs, subject3_figures, failed_trials3 = ph.full_pipline(name="Netta Demo", **pipline_config)
-
-del pipline_config, subject1_figures, failed_trials1, subject2_figures, failed_trials2, subject3_figures, failed_trials3
-
-# subject1 = ph.load_subject(subject_id=1, verbose=True)
-# subject2 = ph.load_subject(subject_id=2, verbose=True)
-# subject3 = ph.load_subject(subject_id=3, verbose=True)
+# subject1 = ph.load_or_preprocess_subject(name_or_id=1, verbose=True)
+# subject2 = ph.load_or_preprocess_subject(name_or_id=2, verbose=True)
+# subject3 = ph.load_or_preprocess_subject(name_or_id=3, verbose=True)
 
 
 ##########################################
